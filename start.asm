1fdddc482e2cd9bf5d8465688a0d9dae8f8d9999
Kevin Lange <kevin.lange@dakko.us>
2011-01-17 17:11:21 -0600

    [make] Clean up a bunch of stuff (thanks Dave)

--- start.asm
+++ start.asm
@@ -1,32 +1,21 @@
 [BITS 32]
-global start
-start:
-	mov esp, _sys_stack
-	jmp stublet
-
 ALIGN 4
 mboot:
 	MULTIBOOT_PAGE_ALIGN	equ 1<<0
 	MULTIBOOT_MEMORY_INFO	equ 1<<1
-	MULTIBOOT_AOUT_KLUDGE	equ 1<<16
 	MULTIBOOT_HEADER_MAGIC	equ 0x1BADB002
-	MULTIBOOT_HEADER_FLAGS	equ MULTIBOOT_PAGE_ALIGN | MULTIBOOT_MEMORY_INFO | MULTIBOOT_AOUT_KLUDGE
+	MULTIBOOT_HEADER_FLAGS	equ MULTIBOOT_PAGE_ALIGN | MULTIBOOT_MEMORY_INFO
 	MULTIBOOT_CHECKSUM		equ -(MULTIBOOT_HEADER_MAGIC + MULTIBOOT_HEADER_FLAGS)
 	EXTERN code, bss, end
 	; GRUB Multiboot header, boot signature
 	dd MULTIBOOT_HEADER_MAGIC
 	dd MULTIBOOT_HEADER_FLAGS
 	dd MULTIBOOT_CHECKSUM
-	; AOUT kludge (must be physical addresses)
-	; Linker script fills these in
-	dd mboot
-	dd code
-	dd bss
-	dd end
-	dd start
 
 ; Main entrypoint
-stublet:
+global start
+start:
+	mov esp, _sys_stack
 	extern	main
 	call	main
 	jmp		$
