e74247cd18c2c1b4613e5d05b9b38a58b3383c93
Kevin Lange <kevin.lange@dakko.us>
2011-02-20 21:10:30 -0600

    [boot] Print in Stage 2 with BIOS interrupts?

--- bootloader/stage2.c
+++ bootloader/stage2.c
@@ -13,3 +13,16 @@ __asm__(".code16\n");
 int main() {
 	return 0;
 }
+
+void print(char * str) {
+	__asm__ ("movb $0x0E, %ah\n"
+			 "movb $0x00, %bh\n"
+			 "movb $0x07, %bl\n"
+			);
+	int i = 0;
+	while (str[i] != '\0') {
+		__asm__ __volatile__ ("movb %0, %%al\n"
+							  "int $0x10" : : "m" (str[i]));
+		++i;
+	}
+}
