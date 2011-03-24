0363ffdf341eb63f66a39faaa219a0298fda7b9d
Kevin Lange <kevin.lange@dakko.us>
2011-03-24 18:24:24 -0500

    [various pieces of garbage]

---
+++ kernel/include/tss.h
@@ -0,0 +1,40 @@
+/*
+ * vim:tabstop=4
+ * vim:noexpandtab
+ */
+#ifndef _TSS_H
+#define _TSS_H
+
+extern void tss_flush();
+
+struct tss_entry_s {
+	uintptr_t previous;
+	uint32_t  esp0;
+	uint32_t  ss0;
+	uint32_t  esp1;
+	uint32_t  ss1;
+	uint32_t  esp2;
+	uint32_t  ss2;
+	uint32_t  cr3;
+	uint32_t  eip;
+	uint32_t  eflags;
+	uint32_t  eax;
+	uint32_t  ecx;
+	uint32_t  edx;
+	uint32_t  ebx;
+	uint32_t  esp;
+	uint32_t  ebp;
+	uint32_t  esi;
+	uint32_t  edi;
+	uint32_t  es;
+	uint32_t  cs;
+	uint32_t  ss;
+	uint32_t  ds;
+	uint32_t  fs;
+	uint32_t  gs;
+	uint32_t  ldt;
+	uint16_t  trap;
+	uint16_t  iomap_base;
+} __attribute__ ((packed));
+
+#endif /* _TSS_H */
--- kernel/include/types.h
+++ kernel/include/types.h
@@ -11,6 +11,7 @@ typedef unsigned long uintptr_t;
 typedef unsigned short uintptr_t;
 #endif
 typedef unsigned long size_t;
+typedef signed int int32_t;
 typedef unsigned int uint32_t;
 typedef unsigned short uint16_t;
 typedef unsigned char uint8_t;
