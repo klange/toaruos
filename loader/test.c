386b28e6fc2f0a1886f66e310cc238523ad7589a
Kevin Lange <kevin.lange@dakko.us>
2011-04-08 12:17:32 -0500

    [test] Make that a Hello World.

--- loader/test.c
+++ loader/test.c
@@ -1,7 +1,10 @@
-void test(int * i) {
-    *i = 5;
+int syscall_print(const char * p1) {
+	int a = 0xA5ADFACE;
+	__asm__ __volatile__("int $0x7F" : "=a" (a) : "0" (0), "b" ((int)p1));
+	return a;
 }
 
 int main(int argc, char ** argv) {
+    syscall_print("Hello world!");
     return 0;
 }
