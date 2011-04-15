332920fe2d795d90b2d37f22e31640839eae8998
Kevin Lange <kevin.lange@dakko.us>
2011-04-15 16:31:58 -0500

    [bin] Some minor updates to the test apps

--- loader/echo.c
+++ loader/echo.c
@@ -1,13 +1,66 @@
 #include <syscall.h>
 
+/* I really need a standard library */
+int
+strlen(
+		const char *str
+	  ) {
+	int i = 0;
+	while (str[i] != (char)0) {
+		++i;
+	}
+	return i;
+}
+
+void usage() {
+	syscall_print("echo [-n] [-e] [STRING]...\n"
+				"  -n    do not output a new line at the end\n"
+				"  -e    process escape sequences\n");
+}
+
 int main(int argc, char ** argv) {
-	for (int i = 1; i < argc; ++i) {
+	int start           = 1;
+	int use_newline     = 1;
+	int process_escapes = 0;
+
+	for (int i = start; i < argc; ++i) {
+		if (argv[i][0] != '-') {
+			start = i;
+			break;
+		} else {
+			if (argv[i][1] == 'h') {
+				usage();
+				return 1;
+			} else if (argv[i][1] == 'n') {
+				use_newline = 0;
+			} else if (argv[i][1] == 'e') {
+				process_escapes = 1;
+			}
+		}
+	}
+
+	for (int i = start; i < argc; ++i) {
+		if (process_escapes) {
+			for (int j = 0; j < strlen(argv[i]) - 1; ++j) {
+				if (argv[i][j] == '\\') {
+					if (argv[i][j+1] == 'e') {
+						argv[i][j] = '\033';
+						for (int k = j + 1; k < strlen(argv[i]); ++k) {
+							argv[i][k] = argv[i][k+1];
+						}
+					}
+				}
+			}
+		}
 		syscall_print(argv[i]);
 		if (i != argc - 1) {
 			syscall_print(" ");
 		}
 	}
-	syscall_print("\n");
+
+	if (use_newline) {
+		syscall_print("\n");
+	}
 	return 0;
 }
 
