3a913fb6c3cd8a5d4428c045317bd2140aae4010
Kevin Lange <kevin.lange@dakko.us>
2011-04-14 20:49:36 -0500

    [fpu] Docs

--- README.md
+++ README.md
@@ -125,3 +125,10 @@ ToAruOS is under the NCSA license, which is a derivative (and fully compatible w
     LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING
     FROM, OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS
     WITH THE SOFTWARE.
+
+### Additional Licenses ###
+
+ToAruOS contains additional software with the following copyright notices:
+
+* The CPU detection code provided in `kernel/core/cpudet.c`:
+  Copyright (c) 2006-2007 -  http://brynet.biz.tm - <brynet@gmail.com>
--- kernel/core/fpu.c
+++ kernel/core/fpu.c
@@ -1,20 +1,40 @@
 /*
- * vim:tabstop=4
- * vim:noexpandtab
+ * Tiny FPU enable module.
+ * 
+ * Part of the ToAruOS Kernel
+ * (C) 2011 Kevin Lange ...
+ * To whatever possible level this short of a code chunk
+ * can be considered copyrightable in your jurisdiction.
  */
 #include <system.h>
 
+/**
+ * Set the FPU control word
+ *
+ * @param cw What to set the control word to.
+ */
 void
 set_fpu_cw(const uint16_t cw) {
 	__asm__ __volatile__("fldcw %0" :: "m"(cw));
 }
 
+/**
+ * Enable the FPU
+ *
+ * We are assuming that we have one to begin with, but since we
+ * only really operate on 686 machines, we do, so we're not
+ * going to bother checking.
+ */
 void
 enable_fpu() {
 	size_t cr4;
-	/* Trust me, we have an FPU */
 	__asm__ __volatile__ ("mov %%cr4, %0" : "=r"(cr4));
 	cr4 |= 0x200;
 	__asm__ __volatile__ ("mov %0, %%cr4" :: "r"(cr4));
 	set_fpu_cw(0x37F);
 }
+
+/*
+ * vim:tabstop=4
+ * vim:noexpandtab
+ */
