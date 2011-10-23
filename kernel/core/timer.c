d8cbf27e08588d179c7d88ebe4961d97ba07080e
Kevin Lange <kevin.lange@dakko.us>
2011-10-22 19:17:16 -0500

    [timer] Comment and decruftify the PIT driver

--- kernel/core/timer.c
+++ kernel/core/timer.c
@@ -1,18 +1,37 @@
 #include <system.h>
 
+#define PIT_A 0x40
+#define PIT_B 0x41
+#define PIT_C 0x42
+#define PIT_CONTROL 0x43
+
+#define PIT_MASK 0xFF
+#define PIT_SCALE 1193180
+#define PIT_SET 0x36
+
+/*
+ * Set the phase (in hertz) for the Programmable
+ * Interrupt Timer (PIT).
+ */
 void
 timer_phase(
 		int hz
 		) {
-	int divisor = 1193180 / hz;
-	outportb(0x43, 0x36);
-	outportb(0x40, divisor & 0xFF);
-	outportb(0x40, (divisor >> 8) & 0xFF);
+	int divisor = PIT_SCALE / hz;
+	outportb(PIT_CONTROL, PIT_SET);
+	outportb(PIT_A, divisor & PIT_MASK);
+	outportb(PIT_A, (divisor >> 8) & PIT_MASK);
 }
 
+/*
+ * Internal timer counters
+ */
 long timer_ticks = 0;
 unsigned long ticker = 0;
 
+/*
+ * IRQ handler for when the timer fires
+ */
 void
 timer_handler(
 		struct regs *r
@@ -21,16 +40,27 @@ timer_handler(
 	switch_task();
 }
 
+/*
+ * Device installer for the PIT
+ */
 void timer_install() {
 	irq_install_handler(0, timer_handler);
 	timer_phase(100); /* 100Hz */
 }
 
+/*
+ * Wait until `ticks` calls to the timer
+ * handler have happened, then resume execution.
+ */
 void
 timer_wait(
 		int ticks
 		) {
+	/* end tick count */
 	long eticks;
 	eticks = (long)timer_ticks + (long)ticks;
-	while(timer_ticks < eticks);
+	while(timer_ticks < eticks) {
+		/* Halt for interrupt */
+		__asm__ __volatile__("hlt");
+	}
 }
