d52e73e71e02e0b04ad2799ca487935810e27325
Kevin Lange <kevin.lange@dakko.us>
2011-12-06 19:46:35 -0600

    [esh] Esh, the experimental shell

---
+++ userspace/esh.c
@@ -0,0 +1,49 @@
+/*
+ * E-Shell
+ *
+ * Test shell for ToAruOS
+ */
+
+#include <stdio.h>
+#include <stdint.h>
+#include <syscall.h>
+#include <string.h>
+#include <stdlib.h>
+#include <unistd.h>
+
+int main(int argc, char ** argv) {
+	printf("I am pid %d\n", getpid());
+	char cwd[1024] = {'/',0};
+	int  pid = getpid();
+	while (1) {
+		char cmd[1024];
+		printf("%s$ ", cwd);
+		fflush(stdout);
+		fgets(cmd, 1024, stdin);
+		cmd[strlen(cmd)-1] = '\0';
+		printf("[%s]\n", cmd);
+		char *p, *tokens[512], *last;
+		int i = 0;
+		for ((p = strtok_r(cmd, " ", &last)); p;
+				(p = strtok_r(NULL, " ", &last)), i++) {
+			if (i < 511) tokens[i] = p;
+		}
+		tokens[i] = NULL;
+		for (uint32_t j = 0; j < i; ++j) {
+			printf("> %s\n", tokens[j]);
+		}
+		if (!strcmp(tokens[0],"exit")) {
+			goto exit;
+		}
+		uint32_t f = fork();
+		if (getpid() != pid) {
+			printf("Executing %s!\n", tokens[0]);
+			execve(tokens[0], tokens, NULL);
+		} else {
+			printf("Waiting for process (%d) to finish.\n", f);
+		}
+	}
+exit:
+
+	return 0;
+}
