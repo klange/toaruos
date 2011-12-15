20fab345a8f8a0af78ca89c27bd089c8c2272ebb
Kevin Lange <kevin.lange@dakko.us>
2011-12-14 20:06:09 -0600

    [user] Simple little test app that triggers a stack overflow

---
+++ userspace/stack-overflow.c
@@ -0,0 +1,19 @@
+/* vim: tabstop=4 shiftwidth=4 noexpandtab
+ *
+ * Sample application which triggers a stack overflow
+ * by means of a simple infinitely recursive function.
+ */
+#include <stdio.h>
+#include <syscall.h>
+
+void overflow() {
+	int i[1024] = {0xff};
+	printf("Stack is at 0x%x\n", &i);
+	overflow();
+}
+
+int main(int argc, char ** argv) {
+	overflow();
+
+	return 0;
+}
