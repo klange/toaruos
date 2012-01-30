ab1958632c0fba717ba2d2c40e952630e60d60f1
Kevin Lange <kevin.lange@dakko.us>
2012-01-30 13:29:13 -0600

    hostname utility

---
+++ userspace/hostname.c
@@ -0,0 +1,37 @@
+/* vim: tabstop=4 shiftwidth=4 noexpandtab
+ *
+ * hostname
+ */
+#include <stdio.h>
+#include <syscall.h>
+
+DEFN_SYSCALL0(getuid, 23);
+DEFN_SYSCALL1(sethostname, 31, char *)
+DEFN_SYSCALL0(gethostname, 32)
+
+#define ROOT_UID 0
+
+int main(int argc, char * argv[]) {
+	if (argc < 2) {
+		char tmp[256];
+		syscall_gethostname(tmp);
+		printf("%s\n", tmp);
+		return 0;
+	} else {
+		if (syscall_getuid() != ROOT_UID) {
+			fprintf(stderr,"Must be root to set hostname.\n");
+			return 1;
+		} else {
+			syscall_sethostname(argv[1]);
+			FILE * file = fopen("/etc/hostname", "w");
+			if (!file) {
+				return 1;
+			} else {
+				fprintf(file, "%s\n", argv[1]);
+				fclose(file);
+				return 0;
+			}
+		}
+	}
+	return 0;
+}
