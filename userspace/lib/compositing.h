9315c95452ed73db36248f45cab51da4ec30a5ec
Kevin Lange <kevin.lange@dakko.us>
2012-02-10 23:29:08 -0600

    Command formats and numbers

--- userspace/lib/compositing.h
+++ userspace/lib/compositing.h
@@ -3,6 +3,9 @@
  * Compositing and Window Management Library
  */
 
+#ifndef COMPOSITING_H
+#define COMPOSITING_H
+
 #include <stdint.h>
 
 typedef struct {
@@ -35,4 +38,39 @@ typedef struct {
 #define WE_MOUSELEAVE	0x22 /* The mouse has left your window (at the given coordinates) */
 #define WE_MOUSECLICK	0x23 /* A mouse button has been pressed that was not previously pressed */
 #define WE_RESIZE		0x30 /* Your window has been resized */
+#define WE_NEWWINDOW	0x31 /* A new window has been created */
 
+typedef struct {
+	uint16_t wid;		/* or none for new window */
+	uint16_t left;		/* X coordinate */
+	uint16_t top;		/* Y coordinate */
+	uint16_t width;		/* Width of window or region */
+	uint16_t height;	/* Height of window or region */
+} wc_window_t;
+
+typedef struct {
+	uint16_t wid;
+	uint16_t width;
+	uint16_t height;
+} we_window_t;
+
+typedef struct {
+	uint16_t wid;
+	uint16_t key;
+} we_keyboard_t;
+
+typedef struct {
+	uint16_t wid;
+	uint16_t old_x;
+	uint16_t old_y;
+	uint16_t new_x;
+	uint16_t new_y;
+	uint8_t  buttons;
+} we_mouse_t;
+
+#define MOUSE_BUTTON_LEFT		0x01
+#define MOUSE_BUTTON_RIGHT		0x02
+#define MOUSE_BUTTON_MIDDLE		0x04
+
+
+#endif
