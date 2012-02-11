3411eb380f3ed2e8de9c0408268a7a5f360f4964
Kevin Lange <kevin.lange@dakko.us>
2012-02-11 00:06:09 -0600

    Protocol refinements

--- userspace/lib/compositing.h
+++ userspace/lib/compositing.h
@@ -10,11 +10,19 @@
 
 typedef struct {
 	volatile uint8_t lock;			/* Spinlock byte */
+
+	/* LOCK REQUIRED REGION */
 	volatile uint8_t client_done;	/* Client has finished work */
 	volatile uint8_t server_done;	/* Server has finished work */
 	pid_t client_pid;				/* Actively communicating client process */
 	uintptr_t event_pipe;			/* Client event pipe (ie, mouse, keyboard) */
 	uintptr_t command_pipe;			/* Client command pipe (ie, resize) */
+	/* END LOCK REQUIRED REGION */
+
+	uint16_t server_width;			/* Screen resolution, width */
+	uint16_t server_height;			/* Screen resolution, height */
+	uint8_t server_depth;			/* Native screen depth (in bits) */
+
 } wins_server_global_t;
 
 typedef struct {
@@ -26,7 +34,7 @@ typedef struct {
 
 /* Commands */
 #define WC_NEWWINDOW	0x00 /* New Window */
-#define WC_RESIZE		0x01 /* Resize existing window */
+#define WC_RESIZE		0x01 /* Resize and move an existing window */
 #define WC_DESTROY		0x02 /* Destroy an existing window */
 #define WC_DAMAGE		0x03 /* Damage window (redraw region) */
 
@@ -37,8 +45,9 @@ typedef struct {
 #define WE_MOUSEENTER	0x21 /* The mouse has entered your window (at the given coordinates) */
 #define WE_MOUSELEAVE	0x22 /* The mouse has left your window (at the given coordinates) */
 #define WE_MOUSECLICK	0x23 /* A mouse button has been pressed that was not previously pressed */
-#define WE_RESIZE		0x30 /* Your window has been resized */
-#define WE_NEWWINDOW	0x31 /* A new window has been created */
+#define WE_MOUSEUP		0x24 /* A mouse button has been released */
+#define WE_NEWWINDOW	0x30 /* A new window has been created */
+#define WE_RESIZED		0x31 /* Your window has been resized or moved */
 
 typedef struct {
 	uint16_t wid;		/* or none for new window */
@@ -46,18 +55,12 @@ typedef struct {
 	uint16_t top;		/* Y coordinate */
 	uint16_t width;		/* Width of window or region */
 	uint16_t height;	/* Height of window or region */
-} wc_window_t;
-
-typedef struct {
-	uint16_t wid;
-	uint16_t width;
-	uint16_t height;
-} we_window_t;
+} w_window_t;
 
 typedef struct {
 	uint16_t wid;
 	uint16_t key;
-} we_keyboard_t;
+} w_keyboard_t;
 
 typedef struct {
 	uint16_t wid;
@@ -66,7 +69,7 @@ typedef struct {
 	uint16_t new_x;
 	uint16_t new_y;
 	uint8_t  buttons;
-} we_mouse_t;
+} w_mouse_t;
 
 #define MOUSE_BUTTON_LEFT		0x01
 #define MOUSE_BUTTON_RIGHT		0x02
