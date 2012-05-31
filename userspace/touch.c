6dae6ac0a775079f5e531be9b24570d68dee9fdb
Kevin Lange <kevin.lange@dakko.us>
2012-05-31 20:37:16 +0900

    Add a simple touch command (not exactly compliant)

---
+++ userspace/touch.c
@@ -0,0 +1,14 @@
+#include <stdio.h>
+#include <string.h>
+
+int main(int argc, char * argv[]) {
+	if (argc < 2) {
+		fprintf(stderr, "%s: argument expected\n", argv[0]);
+		return 1;
+	}
+
+	FILE * f = fopen(argv[1], "w");
+	fclose(f);
+
+	return 0;
+}
