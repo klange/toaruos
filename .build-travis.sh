7bcc2e1264eca242d16c3b3a8d4b53b386ea8f3c
Kevin Lange <kevin.lange@dakko.us>
2012-09-03 23:19:18 -0700

    Actually fix travis.

--- .build-travis.sh
+++ .build-travis.sh
@@ -4,7 +4,7 @@ unset CC
 sudo apt-get install clang yasm genext2fs build-essential wget libmpfr-dev libmpc-dev libgmp-dev qemu autoconf automake texinfo
 sudo apt-get remove kvm-ipxe
 mkdir ~/bin
-ln -s `which qemu-system-i386` ~/bin
+ln -s `which qemu-system-i386` ~/bin/qemu
 export PATH="~/bin:$PATH"
 
 # Build the toolchain: (silently)
