39aca690d68cba66dada8cafaa0369e06e1c29ce
Kevin Lange <kevin.lange@dakko.us>
2012-12-07 20:23:41 -0800

    replace toolchain build with precompiled

--- .before_script.sh
+++ .before_script.sh
@@ -3,8 +3,14 @@
 # Travis before_script
 unset CC
 
+sudo mkdir -p /home/build/osdev/toolchain
+
 pushd toolchain
-./prepare.sh  || exit 1
-./install.sh  > /dev/null 2> /dev/null || exit 1
-. activate.sh || exit 1
+    cp activate.sh /home/build/osdev/toolchain/
+popd
+
+pushd /home/build/osdev/toolchain
+    wget "https://github.com/downloads/klange/osdev/toolchain-2012-12-07.tar.gz"
+    tar -xaf "toolchain-2012-12-07.tar.gz"
+    . activate.sh || exit 1
 popd
