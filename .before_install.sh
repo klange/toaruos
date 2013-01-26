19890c748e548e1cf9a74e48024f924143ec12d4
Kevin Lange <kevin.lange@dakko.us>
2013-01-25 23:34:26 -0800

    eh... try to fix travis

--- .before_install.sh
+++ .before_install.sh
@@ -1,6 +1,7 @@
 #!/bin/bash
 
 # Travis before_install script
+sudo apt-get update
 sudo apt-get install clang yasm genext2fs build-essential wget libmpfr-dev libmpc-dev libgmp-dev qemu autoconf automake texinfo
 sudo apt-get remove kvm-ipxe
 
