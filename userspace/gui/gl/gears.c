0a140ce9412eb1c6f6004cc29f196e65b86b999d
Kevin Lange <kevin.lange@dakko.us>
2013-04-12 21:20:38 -0700

    gears (a port of glxgears) SEE LICENSE IN FILE

---
+++ userspace/gui/gl/gears.c
@@ -0,0 +1,345 @@
+/*
+ * Copyright (C) 1999-2001  Brian Paul   All Rights Reserved.
+ *                    2013  Kevin Lange <klange@dakko.us>
+ * 
+ * Permission is hereby granted, free of charge, to any person obtaining a
+ * copy of this software and associated documentation files (the "Software"),
+ * to deal in the Software without restriction, including without limitation
+ * the rights to use, copy, modify, merge, publish, distribute, sublicense,
+ * and/or sell copies of the Software, and to permit persons to whom the
+ * Software is furnished to do so, subject to the following conditions:
+ * 
+ * The above copyright notice and this permission notice shall be included
+ * in all copies or substantial portions of the Software.
+ * 
+ * THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS
+ * OR IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
+ * FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT.  IN NO EVENT SHALL
+ * BRIAN PAUL BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER LIABILITY, WHETHER IN
+ * AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM, OUT OF OR IN
+ * CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE SOFTWARE.
+ */
+#include <stdlib.h>
+#include <math.h>
+#include <syscall.h>
+
+#include <GL/gl.h>
+#include <GL/osmesa.h>
+
+#include "lib/window.h"
+#include "lib/graphics.h"
+
+struct timeval {
+	unsigned int tv_sec;
+	unsigned int tv_usec;
+};
+
+static unsigned int frames = 0;
+static unsigned int start_time = 0;
+
+void fps() {
+	struct timeval now;
+	syscall_gettimeofday(&now, NULL); //time(NULL);
+	frames++;
+	if (!start_time) {
+		start_time = now.tv_sec;
+	} else if (now.tv_sec - start_time >= 5) {
+		syscall_gettimeofday(&now, NULL); //time(NULL);
+		GLfloat seconds = now.tv_sec - start_time;
+		GLfloat fps = frames / seconds;
+		printf("%d frames in %3.1f seconds = %6.3f FPS\n", frames, seconds, fps);
+		start_time = now.tv_sec;
+		frames = 0;
+	}
+}
+
+static GLfloat view_rotx = 20.0, view_roty = 30.0, view_rotz = 0.0;
+static GLint gear1, gear2, gear3;
+static GLfloat angle = 0.0;
+
+static void gear(GLfloat inner_radius, GLfloat outer_radius, GLfloat width, GLint teeth, GLfloat tooth_depth) {
+	GLint i;
+	GLfloat r0, r1, r2;
+	GLfloat angle, da;
+	GLfloat u, v, len;
+
+	r0 = inner_radius;
+	r1 = outer_radius - tooth_depth / 2.0;
+	r2 = outer_radius + tooth_depth / 2.0;
+
+	da = 2.0 * M_PI / teeth / 4.0;
+
+	glShadeModel(GL_FLAT);
+
+	glNormal3f(0.0, 0.0, 1.0);
+
+	/* draw front face */
+	glBegin(GL_QUAD_STRIP);
+	for (i = 0; i <= teeth; i++) {
+		angle = i * 2.0 * M_PI / teeth;
+		glVertex3f(r0 * cos(angle), r0 * sin(angle), width * 0.5);
+		glVertex3f(r1 * cos(angle), r1 * sin(angle), width * 0.5);
+		if (i < teeth) {
+			glVertex3f(r0 * cos(angle), r0 * sin(angle), width * 0.5);
+			glVertex3f(r1 * cos(angle + 3 * da), r1 * sin(angle + 3 * da), width * 0.5);
+		}
+	}
+	glEnd();
+
+	/* draw front sides of teeth */
+	glBegin(GL_QUADS);
+	da = 2.0 * M_PI / teeth / 4.0;
+	for (i = 0; i < teeth; i++) {
+		angle = i * 2.0 * M_PI / teeth;
+
+		glVertex3f(r1 * cos(angle), r1 * sin(angle), width * 0.5);
+		glVertex3f(r2 * cos(angle + da), r2 * sin(angle + da), width * 0.5);
+		glVertex3f(r2 * cos(angle + 2 * da), r2 * sin(angle + 2 * da), width * 0.5);
+		glVertex3f(r1 * cos(angle + 3 * da), r1 * sin(angle + 3 * da), width * 0.5);
+	}
+	glEnd();
+
+	glNormal3f(0.0, 0.0, -1.0);
+
+	/* draw back face */
+	glBegin(GL_QUAD_STRIP);
+	for (i = 0; i <= teeth; i++) {
+		angle = i * 2.0 * M_PI / teeth;
+		glVertex3f(r1 * cos(angle), r1 * sin(angle), -width * 0.5);
+		glVertex3f(r0 * cos(angle), r0 * sin(angle), -width * 0.5);
+		if (i < teeth) {
+			glVertex3f(r1 * cos(angle + 3 * da), r1 * sin(angle + 3 * da), -width * 0.5);
+			glVertex3f(r0 * cos(angle), r0 * sin(angle), -width * 0.5);
+		}
+	}
+	glEnd();
+
+	/* draw back sides of teeth */
+	glBegin(GL_QUADS);
+	da = 2.0 * M_PI / teeth / 4.0;
+	for (i = 0; i < teeth; i++) {
+		angle = i * 2.0 * M_PI / teeth;
+
+		glVertex3f(r1 * cos(angle + 3 * da), r1 * sin(angle + 3 * da), -width * 0.5);
+		glVertex3f(r2 * cos(angle + 2 * da), r2 * sin(angle + 2 * da), -width * 0.5);
+		glVertex3f(r2 * cos(angle + da), r2 * sin(angle + da), -width * 0.5);
+		glVertex3f(r1 * cos(angle), r1 * sin(angle), -width * 0.5);
+	}
+	glEnd();
+
+	/* draw outward faces of teeth */
+	glBegin(GL_QUAD_STRIP);
+	for (i = 0; i < teeth; i++) {
+		angle = i * 2.0 * M_PI / teeth;
+
+		glVertex3f(r1 * cos(angle), r1 * sin(angle), width * 0.5);
+		glVertex3f(r1 * cos(angle), r1 * sin(angle), -width * 0.5);
+		u = r2 * cos(angle + da) - r1 * cos(angle);
+		v = r2 * sin(angle + da) - r1 * sin(angle);
+		len = sqrt(u * u + v * v);
+		u /= len;
+		v /= len;
+		glNormal3f(v, -u, 0.0);
+		glVertex3f(r2 * cos(angle + da), r2 * sin(angle + da), width * 0.5);
+		glVertex3f(r2 * cos(angle + da), r2 * sin(angle + da), -width * 0.5);
+		glNormal3f(cos(angle), sin(angle), 0.0);
+		glVertex3f(r2 * cos(angle + 2 * da), r2 * sin(angle + 2 * da),  width * 0.5);
+		glVertex3f(r2 * cos(angle + 2 * da), r2 * sin(angle + 2 * da), -width * 0.5);
+		u = r1 * cos(angle + 3 * da) - r2 * cos(angle + 2 * da);
+		v = r1 * sin(angle + 3 * da) - r2 * sin(angle + 2 * da);
+		glNormal3f(v, -u, 0.0);
+		glVertex3f(r1 * cos(angle + 3 * da), r1 * sin(angle + 3 * da), width * 0.5);
+		glVertex3f(r1 * cos(angle + 3 * da), r1 * sin(angle + 3 * da), -width * 0.5);
+		glNormal3f(cos(angle), sin(angle), 0.0);
+	}
+
+	glVertex3f(r1 * cos(0), r1 * sin(0), width * 0.5);
+	glVertex3f(r1 * cos(0), r1 * sin(0), -width * 0.5);
+
+	glEnd();
+
+	glShadeModel(GL_SMOOTH);
+
+	/* draw inside radius cylinder */
+	glBegin(GL_QUAD_STRIP);
+	for (i = 0; i <= teeth; i++) {
+		angle = i * 2.0 * M_PI / teeth;
+		glNormal3f(-cos(angle), -sin(angle), 0.0);
+		glVertex3f(r0 * cos(angle), r0 * sin(angle), -width * 0.5);
+		glVertex3f(r0 * cos(angle), r0 * sin(angle), width * 0.5);
+	}
+	glEnd();
+}
+
+static void draw(void) {
+	glClear(GL_COLOR_BUFFER_BIT | GL_DEPTH_BUFFER_BIT);
+
+	glPushMatrix();
+	glRotatef(view_rotx, 1.0, 0.0, 0.0);
+	glRotatef(view_roty, 0.0, 1.0, 0.0);
+	glRotatef(view_rotz, 0.0, 0.0, 1.0);
+
+	glPushMatrix();
+	glTranslatef(-3.0, -2.0, 0.0);
+	glRotatef(angle, 0.0, 0.0, 1.0);
+	glCallList(gear1);
+	glPopMatrix();
+
+	glPushMatrix();
+	glTranslatef(3.1, -2.0, 0.0);
+	glRotatef(-2.0 * angle - 9.0, 0.0, 0.0, 1.0);
+	glCallList(gear2);
+	glPopMatrix();
+
+	glPushMatrix();
+	glTranslatef(-3.1, 4.2, 0.0);
+	glRotatef(-2.0 * angle - 25.0, 0.0, 0.0, 1.0);
+	glCallList(gear3);
+	glPopMatrix();
+
+	glPopMatrix();
+}
+
+/* new window size or exposure */
+static void reshape(int width, int height) {
+	GLfloat h = (GLfloat) height / (GLfloat) width;
+
+	glViewport(0, 0, (GLint) width, (GLint) height);
+	glMatrixMode(GL_PROJECTION);
+	glLoadIdentity();
+	glFrustum(-1.0, 1.0, -h, h, 5.0, 60.0);
+	glMatrixMode(GL_MODELVIEW);
+	glLoadIdentity();
+	glTranslatef(0.0, 0.0, -40.0);
+}
+
+static void init(void) {
+	static GLfloat pos[4] = { 5.0, 5.0, 10.0, 0.0 };
+	static GLfloat red[4] = { 0.8, 0.1, 0.0, 1.0 };
+	static GLfloat green[4] = { 0.0, 0.8, 0.2, 1.0 };
+	static GLfloat blue[4] = { 0.2, 0.2, 1.0, 1.0 };
+
+	glLightfv(GL_LIGHT0, GL_POSITION, pos);
+	glEnable(GL_CULL_FACE);
+	glEnable(GL_LIGHTING);
+	glEnable(GL_LIGHT0);
+	glEnable(GL_DEPTH_TEST);
+
+	/* make the gears */
+	gear1 = glGenLists(1);
+	glNewList(gear1, GL_COMPILE);
+	glMaterialfv(GL_FRONT, GL_AMBIENT_AND_DIFFUSE, red);
+	gear(1.0, 4.0, 1.0, 20, 0.7);
+	glEndList();
+
+	gear2 = glGenLists(1);
+	glNewList(gear2, GL_COMPILE);
+	glMaterialfv(GL_FRONT, GL_AMBIENT_AND_DIFFUSE, green);
+	gear(0.5, 2.0, 2.0, 10, 0.7);
+	glEndList();
+
+	gear3 = glGenLists(1);
+	glNewList(gear3, GL_COMPILE);
+	glMaterialfv(GL_FRONT, GL_AMBIENT_AND_DIFFUSE, blue);
+	gear(1.3, 2.0, 0.5, 10, 0.7);
+	glEndList();
+
+	glEnable(GL_NORMALIZE);
+}
+
+int resize(gfx_context_t * ctx, OSMesaContext gl_ctx) {
+
+	if (!OSMesaMakeCurrent(gl_ctx, ctx->backbuffer, GL_UNSIGNED_BYTE, ctx->width, ctx->height))
+		return 1;
+
+	OSMesaPixelStore(OSMESA_Y_UP, 0);
+
+	reshape(ctx->width, ctx->height);
+	return 0;
+}
+
+int main (int argc, char ** argv) {
+	int left = 30;
+	int top  = 30;
+
+	int width  = 500;
+	int height = 500;
+	int quit = 0;
+
+
+	setup_windowing();
+
+	/* Do something with a window */
+	window_t * wina = window_create(left, top, width, height);
+	gfx_context_t * ctx = init_graphics_window_double_buffer(wina);
+	draw_fill(ctx, rgb(0,0,0));
+	win_sane_events();
+
+	OSMesaContext gl_ctx = OSMesaCreateContext(OSMESA_BGRA, NULL);
+	if (resize(ctx, gl_ctx)) {
+		fprintf(stderr, "%s: Something bad happened.\n", argv[0]);
+		goto finish;
+	}
+
+	init();
+
+	while (!quit) {
+		wins_packet_t * event = get_window_events_async();
+		if (event) {
+			switch (event->command_type & WE_GROUP_MASK) {
+				case WE_WINDOW_EVT: {
+					w_window_t * evt = (w_window_t *)((uintptr_t)event + sizeof(wins_packet_t));
+					window_t * window = NULL;
+					switch (event->command_type) {
+						case WE_RESIZED:
+							window = wins_get_window(evt->wid);
+							if (window) {
+								/* Accept resize request */
+								resize_window_buffer_client(window, evt->left, evt->top, evt->width, evt->height);
+								/* Reinitialize core graphics library */
+								reinit_graphics_window(ctx, wina);
+								/* Fix up the GL context as well */
+								resize(ctx, gl_ctx);
+							}
+							break;
+					}
+					break;
+				}
+				case WE_KEY_EVT: {
+					w_keyboard_t * kbd = (w_keyboard_t *)((uintptr_t)event + sizeof(wins_packet_t));
+					switch (kbd->event.keycode) {
+						case 'q':
+							free(event);
+							goto finish;
+						case KEY_ARROW_LEFT:
+							view_roty += 5.0;
+							break;
+						case KEY_ARROW_RIGHT:
+							view_roty -= 5.0;
+							break;
+						case KEY_ARROW_UP:
+							view_rotx += 5.0;
+							break;
+						case KEY_ARROW_DOWN:
+							view_rotx -= 5.0;
+							break;
+					}
+					break;
+				}
+			}
+			free(event);
+		}
+
+		fps();
+		angle += 0.2;
+		draw();
+		flip(ctx);
+		syscall_yield();
+	}
+
+finish:
+	OSMesaDestroyContext(gl_ctx);
+	teardown_windowing();
+
+	return 0;
+}
