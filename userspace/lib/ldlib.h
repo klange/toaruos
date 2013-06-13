ea52ca3fb0edc87f0022a9a89ef468f23d6c5711
Kevin Lange <kevin.lange@dakko.us>
2013-06-12 17:52:15 -0700

    kill old unused stuff

--- userspace/lib/ldlib.h
+++
@@ -1,8 +0,0 @@
-#ifndef LDLIB_H
-#define LDLIB_H
-
-#include <syscall.h>
-#include <stdint.h>
-#include <unistd.h>
-
-#endif
--- userspace/lib/syscall.h
+++
@@ -1,8 +0,0 @@
-/* vim: tabstop=4 shiftwidth=4 noexpandtab
- */
-#ifndef LIB_GRAPHICS_H
-#define LIB_GRAPHICS_H
-
-
-
-#endif
