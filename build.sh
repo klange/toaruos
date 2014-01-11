e2fc901d9373579650b848ac1b7a936f74dcf083
Kevin Lange <kevin.lange@dakko.us>
2014-01-10 18:29:55 -0800

    Make sure we've got a good genext2fs

--- build.sh
+++ build.sh
@@ -27,6 +27,23 @@ else
     echo "... then comment out the 'exit' below this block of echos in 'build.sh'."
     exit 1
 fi
+
+# Really quick, let's check our genext2fs before we start
+if [ -z "$(genext2fs --help 2>&1 | grep -- "block-size")" ]; then
+    echo -e "\033[1;31mHold up!\033[0m"
+    echo "Your genext2fs does not support the -B (--block-size) argument."
+    echo "You need to build your own genext2fs with the patch described"
+    echo "in this mailing list post:"
+    echo
+    echo "    http://bugs.debian.org/cgi-bin/bugreport.cgi?bug=562999"
+    echo
+    echo "You can get genext2fs sources from sourceforge:"
+    echo
+    echo "    http://genext2fs.sourceforge.net/"
+    echo
+    exit 1
+fi
+
 # Build the toolchain:
 unset PKG_CONFIG_LIBDIR
 pushd toolchain
