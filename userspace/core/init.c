e52728fcba40eb92719d69f4a153441a311ac4d5
Kevin Lange <kevin.lange@dakko.us>
2014-04-12 18:15:44 -0700

    Run fullscreen terminal under yutani

--- userspace/core/init.c
+++ userspace/core/init.c
@@ -65,7 +65,7 @@ int main(int argc, char * argv[]) {
 			args = argv[2];
 		}
 		if (!strcmp(argv[1],"--single")) {
-			return start_options((char *[]){"/bin/compositor","/bin/terminal","-Fl",args,NULL});
+			return start_options((char *[]){"/bin/compositor-beta","--","/bin/terminal-beta","-Fl",args,NULL});
 		} else if (!strcmp(argv[1], "--single-beta")) {
 			return start_options((char *[]){"/bin/compositor-beta",NULL});
 		} else if (!strcmp(argv[1], "--vga")) {
