36feea16735f48fb0903703962034789b04d9023
Kevin Lange <kevin.lange@dakko.us>
2014-06-07 16:20:54 -0700

    Support multiple paths in ls, with more expected output

--- userspace/core/ls.c
+++ userspace/core/ls.c
@@ -43,31 +43,46 @@
 static int human_readable = 0;
 static int stdout_is_tty = 1;
 static int this_year = 0;
+static int explicit_path_set = 0;
+static int show_hidden = 0;
+static int long_mode   = 0;
+static int print_dir   = 0;
+static int term_width = DEFAULT_TERM_WIDTH;
+static int term_height = DEFAULT_TERM_HEIGHT;
 
-int entcmp(const void * c1, const void * c2) {
-	struct dirent * d1 = *(struct dirent **)c1;
-	struct dirent * d2 = *(struct dirent **)c2;
-	return strcmp(d1->d_name, d2->d_name);
+struct tfile {
+	char * name;
+	struct stat statbuf;
+};
+
+static int filecmp(const void * c1, const void * c2) {
+	const struct tfile * d1 = *(const struct tfile **)c1;
+	const struct tfile * d2 = *(const struct tfile **)c2;
+
+	int a = S_ISDIR(d1->statbuf.st_mode);
+	int b = S_ISDIR(d2->statbuf.st_mode);
+
+	if (a == b) return strcmp(d1->name, d2->name);
+	else if (a < b) return -1;
+	else if (a > b) return 1;
 }
 
-void print_entry(const char * filename, const char * srcpath, int colwidth) {
-	/* Figure out full relpath */
-	char * relpath = malloc(strlen(srcpath) + strlen(filename) + 2);
-	sprintf(relpath, "%s/%s", srcpath, filename);
+static int filecmp_notypesort(const void * c1, const void * c2) {
+	const struct tfile * d1 = *(const struct tfile **)c1;
+	const struct tfile * d2 = *(const struct tfile **)c2;
 
-	/* Classify file */
-	struct stat statbuf;
-	stat(relpath, &statbuf);
-	free(relpath);
+	return strcmp(d1->name, d2->name);
+}
 
+static void print_entry(struct tfile * file, int colwidth) {
 	const char * ansi_color_str;
-	if (S_ISDIR(statbuf.st_mode)) {
+	if (S_ISDIR(file->statbuf.st_mode)) {
 		/* Directory */
 		ansi_color_str = DIR_COLOR;
-	} else if (statbuf.st_mode & 0111) {
+	} else if (file->statbuf.st_mode & 0111) {
 		/* Executable */
 		ansi_color_str = EXE_COLOR;
-	} else if (S_ISBLK(statbuf.st_mode) || S_ISCHR(statbuf.st_mode)) {
+	} else if (S_ISBLK(file->statbuf.st_mode) || S_ISCHR(file->statbuf.st_mode)) {
 		/* Device file */
 		ansi_color_str = DEVICE_COLOR;
 	} else {
@@ -78,18 +93,18 @@ void print_entry(const char * filename, const char * srcpath, int colwidth) {
 
 	/* Print the file name */
 	if (stdout_is_tty) {
-		printf("\033[%sm%s\033[0m", ansi_color_str, filename);
+		printf("\033[%sm%s\033[0m", ansi_color_str, file->name);
 	} else {
-		printf("%s", filename);
+		printf("%s", file->name);
 	}
 
 	/* Pad the rest of the column */
-	for (int rem = colwidth - strlen(filename); rem > 0; rem--) {
+	for (int rem = colwidth - strlen(file->name); rem > 0; rem--) {
 		printf(" ");
 	}
 }
 
-int print_username(char * _out, int uid) {
+static int print_username(char * _out, int uid) {
 	FILE * passwd = fopen("/etc/passwd", "r");
 	char line[LINE_LEN];
 	int out;
@@ -116,7 +131,7 @@ int print_username(char * _out, int uid) {
 	return out;
 }
 
-int print_human_readable_size(char * _out, size_t s) {
+static int print_human_readable_size(char * _out, size_t s) {
 	if (s >= 1<<20) {
 		size_t t = s / (1 << 20);
 		return sprintf(_out, "%d.%1dM", t, (s - t * (1 << 20)) / ((1 << 20) / 10));
@@ -128,59 +143,42 @@ int print_human_readable_size(char * _out, size_t s) {
 	}
 }
 
-void update_column_widths(int * widths, const char * filename, const char * srcpath) {
-	char * relpath = malloc(strlen(srcpath) + strlen(filename) + 2);
-	sprintf(relpath, "%s/%s", srcpath, filename);
-
-	/* Classify file */
-	struct stat statbuf;
-	stat(relpath, &statbuf);
-	free(relpath);
-
+static void update_column_widths(int * widths, struct tfile * file) {
 	char tmp[256];
 	int n;
 
 	/* Links */
-	n = sprintf(tmp, "%d", statbuf.st_nlink);
+	n = sprintf(tmp, "%d", file->statbuf.st_nlink);
 	if (n > widths[0]) widths[0] = n;
 
 	/* User */
-	n = print_username(tmp, statbuf.st_uid);
+	n = print_username(tmp, file->statbuf.st_uid);
 	if (n > widths[1]) widths[1] = n;
 
 	/* Group */
-	n = print_username(tmp, statbuf.st_gid);
+	n = print_username(tmp, file->statbuf.st_gid);
 	if (n > widths[2]) widths[2] = n;
 
 	/* File size */
 	if (human_readable) {
-		n = print_human_readable_size(tmp, statbuf.st_size);
+		n = print_human_readable_size(tmp, file->statbuf.st_size);
 	} else {
-		n = sprintf(tmp, "%d", statbuf.st_size);
+		n = sprintf(tmp, "%d", file->statbuf.st_size);
 	}
 	if (n > widths[3]) widths[3] = n;
 }
 
-void print_entry_long(int * widths, const char * filename, const char * srcpath) {
-	/* Figure out full relpath */
-	char * relpath = malloc(strlen(srcpath) + strlen(filename) + 2);
-	sprintf(relpath, "%s/%s", srcpath, filename);
-
-	/* Classify file */
-	struct stat statbuf;
-	stat(relpath, &statbuf);
-	free(relpath);
-
+static void print_entry_long(int * widths, struct tfile * file) {
 	const char * ansi_color_str;
-	if (S_ISDIR(statbuf.st_mode)) {
+	if (S_ISDIR(file->statbuf.st_mode)) {
 		// Directory
 		ansi_color_str = DIR_COLOR;
-	} else if (statbuf.st_mode & S_ISUID) {
+	} else if (file->statbuf.st_mode & S_ISUID) {
 		ansi_color_str = SETUID_COLOR;
-	} else if (statbuf.st_mode & 0111) {
+	} else if (file->statbuf.st_mode & 0111) {
 		// Executable
 		ansi_color_str = EXE_COLOR;
-	} else if (S_ISBLK(statbuf.st_mode) || S_ISCHR(statbuf.st_mode)) {
+	} else if (S_ISBLK(file->statbuf.st_mode) || S_ISCHR(file->statbuf.st_mode)) {
 		/* Device file */
 		ansi_color_str = DEVICE_COLOR;
 	} else {
@@ -189,42 +187,42 @@ void print_entry_long(int * widths, const char * filename, const char * srcpath)
 	}
 
 	/* file permissions */
-	if (S_ISLNK(statbuf.st_mode))       { printf("l"); }
-	else if (S_ISCHR(statbuf.st_mode))  { printf("c"); }
-	else if (S_ISBLK(statbuf.st_mode))  { printf("b"); }
-	else if (S_ISDIR(statbuf.st_mode))  { printf("d"); }
+	if (S_ISLNK(file->statbuf.st_mode))       { printf("l"); }
+	else if (S_ISCHR(file->statbuf.st_mode))  { printf("c"); }
+	else if (S_ISBLK(file->statbuf.st_mode))  { printf("b"); }
+	else if (S_ISDIR(file->statbuf.st_mode))  { printf("d"); }
 	else { printf("-"); }
-	printf( (statbuf.st_mode & S_IRUSR) ? "r" : "-");
-	printf( (statbuf.st_mode & S_IWUSR) ? "w" : "-");
-	if (statbuf.st_mode & S_ISUID) {
+	printf( (file->statbuf.st_mode & S_IRUSR) ? "r" : "-");
+	printf( (file->statbuf.st_mode & S_IWUSR) ? "w" : "-");
+	if (file->statbuf.st_mode & S_ISUID) {
 		printf("s");
 	} else {
-		printf( (statbuf.st_mode & S_IXUSR) ? "x" : "-");
+		printf( (file->statbuf.st_mode & S_IXUSR) ? "x" : "-");
 	}
-	printf( (statbuf.st_mode & S_IRGRP) ? "r" : "-");
-	printf( (statbuf.st_mode & S_IWGRP) ? "w" : "-");
-	printf( (statbuf.st_mode & S_IXGRP) ? "x" : "-");
-	printf( (statbuf.st_mode & S_IROTH) ? "r" : "-");
-	printf( (statbuf.st_mode & S_IWOTH) ? "w" : "-");
-	printf( (statbuf.st_mode & S_IXOTH) ? "x" : "-");
+	printf( (file->statbuf.st_mode & S_IRGRP) ? "r" : "-");
+	printf( (file->statbuf.st_mode & S_IWGRP) ? "w" : "-");
+	printf( (file->statbuf.st_mode & S_IXGRP) ? "x" : "-");
+	printf( (file->statbuf.st_mode & S_IROTH) ? "r" : "-");
+	printf( (file->statbuf.st_mode & S_IWOTH) ? "w" : "-");
+	printf( (file->statbuf.st_mode & S_IXOTH) ? "x" : "-");
 
-	printf( " %*d ", widths[0], statbuf.st_nlink); /* number of links, not supported */
+	printf( " %*d ", widths[0], file->statbuf.st_nlink); /* number of links, not supported */
 
 	char tmp[100];
-	print_username(tmp, statbuf.st_uid);
+	print_username(tmp, file->statbuf.st_uid);
 	printf("%-*s ", widths[1], tmp);
-	print_username(tmp, statbuf.st_gid);
+	print_username(tmp, file->statbuf.st_gid);
 	printf("%-*s ", widths[2], tmp);
 
 	if (human_readable) {
-		print_human_readable_size(tmp, statbuf.st_size);
+		print_human_readable_size(tmp, file->statbuf.st_size);
 		printf("%*s ", widths[3], tmp);
 	} else {
-		printf("%*d ", widths[3], statbuf.st_size);
+		printf("%*d ", widths[3], file->statbuf.st_size);
 	}
 
 	char time_buf[80];
-	struct tm * timeinfo = localtime(&statbuf.st_mtime);
+	struct tm * timeinfo = localtime(&file->statbuf.st_mtime);
 	if (timeinfo->tm_year == this_year) {
 		strftime(time_buf, 80, "%b %d %H:%M", timeinfo);
 	} else {
@@ -234,15 +232,15 @@ void print_entry_long(int * widths, const char * filename, const char * srcpath)
 
 	/* Print the file name */
 	if (stdout_is_tty) {
-		printf("\033[%sm%s\033[0m", ansi_color_str, filename);
+		printf("\033[%sm%s\033[0m", ansi_color_str, file->name);
 	} else {
-		printf("%s", filename);
+		printf("%s", file->name);
 	}
 
 	printf("\n");
 }
 
-void show_usage(int argc, char * argv[]) {
+static void show_usage(int argc, char * argv[]) {
 	printf(
 			"ls - list files\n"
 			"\n"
@@ -255,13 +253,96 @@ void show_usage(int argc, char * argv[]) {
 			"\n", argv[0]);
 }
 
+static void display_tfiles(struct tfile ** ents_array, int numents) {
+	if (long_mode) {
+		int widths[4] = {0,0,0,0};
+		for (int i = 0; i < numents; i++) {
+			update_column_widths(widths, ents_array[i]);
+		}
+		for (int i = 0; i < numents; i++) {
+			print_entry_long(widths, ents_array[i]);
+		}
+	} else {
+		/* Determine the gridding dimensions */
+		int ent_max_len = 0;
+		for (int i = 0; i < numents; i++) {
+			ent_max_len = MAX(ent_max_len, strlen(ents_array[i]->name));
+		}
+
+		int col_ext = ent_max_len + MIN_COL_SPACING;
+		int cols = ((term_width - ent_max_len) / col_ext) + 1;
+
+		/* Print the entries */
+
+		for (int i = 0; i < numents;) {
+
+			/* Columns */
+			print_entry(ents_array[i++], ent_max_len);
+
+			for (int j = 0; (i < numents) && (j < (cols-1)); j++) {
+				printf("  ");
+				print_entry(ents_array[i++], ent_max_len);
+			}
+
+			printf("\n");
+		}
+	}
+}
+
+static int display_dir(char * p) {
+	/* Open the directory */
+	DIR * dirp = opendir(p);
+	if (dirp == NULL) {
+		return 2;
+	}
+
+	if (print_dir) {
+		printf("%s:\n", p);
+	}
+
+	/* Read the entries in the directory */
+	list_t * ents_list = list_create();
+
+	struct dirent * ent = readdir(dirp);
+	while (ent != NULL) {
+		if (show_hidden || (ent->d_name[0] != '.')) {
+			struct tfile * f = malloc(sizeof(struct tfile));
+
+			f->name = strdup(ent->d_name);
+
+			char tmp[strlen(p)+strlen(ent->d_name)+1];
+			sprintf(tmp, "%s/%s", p, ent->d_name);
+			int t = stat(tmp, &f->statbuf);
+
+			list_insert(ents_list, (void *)f);
+		}
+		ent = readdir(dirp);
+	}
+	closedir(dirp);
+
+	/* Now, copy those entries into an array (for sorting) */
+
+	struct tfile ** file_arr = malloc(sizeof(struct tfile *) * ents_list->length);
+	int index = 0;
+	foreach(node, ents_list) {
+		file_arr[index++] = (struct tfile *)node->value;
+	}
+
+	list_free(ents_list);
+
+	qsort(file_arr, index, sizeof(struct tfile *), filecmp_notypesort);
+
+	display_tfiles(file_arr, index);
+
+	free(file_arr);
+
+	return 0;
+}
+
 int main (int argc, char * argv[]) {
 
 	/* Parse arguments */
 	char * p = ".";
-	int explicit_path_set = 0;
-	int show_hidden = 0;
-	int long_mode   = 0;
 
 	if (argc > 1) {
 		int index, c;
@@ -285,97 +366,86 @@ int main (int argc, char * argv[]) {
 		if (optind < argc) {
 			p = argv[optind];
 		}
+		if (optind + 1 < argc) {
+			print_dir = 1;
+		}
 	}
 
 	stdout_is_tty = isatty(STDOUT_FILENO);
 
-	/* Open the directory */
-	DIR * dirp = opendir(p);
-	if (dirp == NULL) {
-		printf("no such directory\n");
-		return -1;
-	}
-
-	/* Read the entries in the directory */
-	list_t * ents_list = list_create();
-
-	struct dirent * ent = readdir(dirp);
-	while (ent != NULL) {
-		if (show_hidden || (ent->d_name[0] != '.')) {
-			struct dirent * entcpy = malloc(sizeof(struct dirent));
-			memcpy(entcpy, ent, sizeof(struct dirent));
-			list_insert(ents_list, (void *)entcpy);
-		}
-
-		ent = readdir(dirp);
-	}
-	closedir(dirp);
-
-	/* Now, copy those entries into an array (for sorting) */
-	struct dirent ** ents_array = malloc(sizeof(struct dirent *) * ents_list->length);
-	int index = 0;
-	node_t * node;
-	foreach(node, ents_list) {
-		ents_array[index++] = (struct dirent *)node->value;
-	}
-	list_free(ents_list);
-	int numents = index;
-
-	qsort(ents_array, numents, sizeof(struct dirent *), entcmp);
-
 	if (long_mode) {
-		int widths[4] = {0,0,0,0};
 		struct tm * timeinfo;
 		struct timeval now;
 		gettimeofday(&now, NULL); //time(NULL);
 		timeinfo = localtime((time_t *)&now.tv_sec);
 		this_year = timeinfo->tm_year;
-		for (int i = 0; i < numents; i++) {
-			update_column_widths(widths, ents_array[i]->d_name, p);
-		}
-		for (int i = 0; i < numents; i++) {
-			print_entry_long(widths, ents_array[i]->d_name, p);
-		}
+	}
+
+	if (stdout_is_tty) {
+		struct winsize w;
+		ioctl(1, TIOCGWINSZ, &w);
+		term_width = w.ws_col;
+		term_height = w.ws_row;
+		term_width -= 1; /* And this just helps clean up our math */
+	}
+
+	int out = 0;
+
+	if (argc == 1 || optind == argc) {
+		display_dir(p);
 	} else {
-		/* Determine the gridding dimensions */
-		int ent_max_len = 0;
-		for (int i = 0; i < numents; i++) {
-			ent_max_len = MAX(ent_max_len, strlen(ents_array[i]->d_name));
-		}
+		list_t * files = list_create();
+		while (p) {
+			struct tfile * f = malloc(sizeof(struct tfile));
 
-		int term_width = DEFAULT_TERM_WIDTH;
-		int term_height = DEFAULT_TERM_HEIGHT;
+			f->name = p;
+			int t = stat(p, &f->statbuf);
 
-		if (stdout_is_tty) {
-			struct winsize w;
-			ioctl(1, TIOCGWINSZ, &w);
-			term_width = w.ws_col;
-			term_height = w.ws_row;
-			term_width -= 1; /* And this just helps clean up our math */
-		}
-
-		int col_ext = ent_max_len + MIN_COL_SPACING;
-		int cols = ((term_width - ent_max_len) / col_ext) + 1;
-
-		/* Print the entries */
-
-		for (int i = 0; i < numents;) {
-
-			/* Columns */
-			print_entry(ents_array[i++]->d_name, p, ent_max_len);
-
-			for (int j = 0; (i < numents) && (j < (cols-1)); j++) {
-				printf("  ");
-				print_entry(ents_array[i++]->d_name, p, ent_max_len);
+			if (t < 0) {
+				printf("ls: cannot access %s: No such file or directory\n", p);
+				free(f);
+				out = 2;
 			}
 
-			printf("\n");
+			list_insert(files, f);
+
+			optind++;
+			if (optind >= argc) p = NULL;
+			else p = argv[optind];
+		}
+
+		struct tfile ** file_arr = malloc(sizeof(struct tfile *) * files->length);
+		int index = 0;
+		foreach(node, files) {
+			file_arr[index++] = (struct tfile *)node->value;
+		}
+
+		list_free(files);
+
+		qsort(file_arr, index, sizeof(struct tfile *), filecmp);
+
+		int first_directory = index;
+
+		for (int i = 0; i < index; ++i) {
+			if (S_ISDIR(file_arr[i]->statbuf.st_mode)) {
+				first_directory = i;
+				break;
+			}
+		}
+
+		if (first_directory) {
+			display_tfiles(file_arr, first_directory);
+		}
+
+		for (int i = first_directory; i < index; ++i) {
+			if ((i != 0) && (i != index-1)) {
+				printf("\n");
+			}
+			display_dir(file_arr[i]->name);
 		}
 	}
 
-	free(ents_array);
-
-	return 0;
+	return out;
 }
 
 /*
