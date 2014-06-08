b58f7ed353ccc710dc312d16d05dd863ca2b5a08
Kevin Lange <kevin.lange@dakko.us>
2014-06-07 23:45:54 -0700

    Strip vim hints from modules

--- modules/random.c
+++ modules/random.c
@@ -1,9 +1,9 @@
 /* This file is part of ToaruOS and is released under the terms
  * of the NCSA / University of Illinois License - see LICENSE.md
  * Copyright (C) 2014 Kevin Lange
- */
-/* vim: tabstop=4 shiftwidth=4 noexpandtab
+ *
  * Provides access to the kernel RNG
+ *
  */
 
 #include <system.h>
--- modules/zero.c
+++ modules/zero.c
@@ -1,9 +1,9 @@
 /* This file is part of ToaruOS and is released under the terms
  * of the NCSA / University of Illinois License - see LICENSE.md
  * Copyright (C) 2014 Kevin Lange
- */
-/* vim: tabstop=4 shiftwidth=4 noexpandtab
+ *
  * Null Device
+ *
  */
 
 #include <system.h>
