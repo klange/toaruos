1ad63b4eaeb3af546013365dae66116666513127
Kevin Lange <kevin.lange@dakko.us>
2014-06-08 00:13:07 -0700

    Dump user into their homedir on login

--- userspace/core/login.c
+++ userspace/core/login.c
@@ -1,8 +1,7 @@
-/* This file is part of ToaruOS and is released under the terms
+/* vim: tabstop=4 shiftwidth=4 noexpandtab
+ * This file is part of ToaruOS and is released under the terms
  * of the NCSA / University of Illinois License - see LICENSE.md
  * Copyright (C) 2013-2014 Kevin Lange
- */
-/* vim: tabstop=4 shiftwidth=4 noexpandtab
  *
  * Login Service
  *
@@ -111,6 +110,7 @@ void set_homedir() {
 		char path[512];
 		sprintf(path,"/home/%s", user);
 		setenv("HOME",path,1);
+		chdir(path);
 	} else {
 		setenv("HOME","/",1);
 	}
