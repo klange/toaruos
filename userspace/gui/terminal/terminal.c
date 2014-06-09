b59871ca67aba7dd4f7dca6d900b8e81df2e9e1d
Kevin Lange <kevin.lange@dakko.us>
2014-06-08 19:47:24 -0700

    More random terminal cleanup while we're here

--- userspace/gui/terminal/terminal.c
+++ userspace/gui/terminal/terminal.c
@@ -1,8 +1,7 @@
-/* This file is part of ToaruOS and is released under the terms
+/* vim: tabstop=4 shiftwidth=4 noexpandtab
+ * This file is part of ToaruOS and is released under the terms
  * of the NCSA / University of Illinois License - see LICENSE.md
  * Copyright (C) 2013-2014 Kevin Lange
- */
-/* vim: tabstop=4 shiftwidth=4 noexpandtab
  *
  * Terminal Emulator
  *
@@ -106,8 +105,6 @@ static void render_decors();
 void term_clear();
 
 void dump_buffer();
-
-wchar_t box_chars[] = L"▒␉␌␍␊°±␤␋┘┐┌└┼⎺⎻─⎼⎽├┤┴┬│≤≥";
 
 /* Trigger to exit the terminal when the child process dies or
  * we otherwise receive an exit signal */
