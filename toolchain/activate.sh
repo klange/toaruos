b946bcad5a0fb7003fa64e514f0aa4f0843a19c3
Kevin Lange <klange@dakko.us>
2015-02-09 23:42:13 -0800

    Fix pkg-config sysroot

--- toolchain/activate.sh
+++ toolchain/activate.sh
@@ -5,4 +5,5 @@ DIR="$( cd "$( dirname "${BASH_SOURCE[0]}" )" && pwd )"
 
 export PATH="$DIR/local/bin:$PATH"
 export PKG_CONFIG_LIBDIR="$TOARU_SYSROOT/usr/lib/pkgconfig"
+export PKG_CONFIG_SYSROOT_DIR="$TOARU_SYSROOT"
 export TOOLCHAIN="$TOARU_SYSROOT/usr"
