1654d371e3962682ae0e516184083628ec753438
Kevin Lange <kevin.lange@dakko.us>
2015-05-20 20:34:20 -0700

    Marking off 0.10.0

--- kernel/sys/version.c
+++ kernel/sys/version.c
@@ -17,7 +17,7 @@ char * __kernel_version_format = "%d.%d.%d-%s";
 
 /* Version numbers X.Y.Z */
 int    __kernel_version_major = 0;
-int    __kernel_version_minor = 9;
+int    __kernel_version_minor = 10;
 int    __kernel_version_lower = 0;
 
 /* Kernel build suffix, which doesn't necessarily
