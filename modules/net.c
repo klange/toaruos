1def54144f955dc3c4be2fb62720036f138bc198
Kevin Lange <klange@dakko.us>
2015-08-07 16:45:10 -0700

    add forum.osdev.org to dns precache

--- modules/net.c
+++ modules/net.c
@@ -992,6 +992,7 @@ static int init(void) {
 	hashmap_set(dns_cache, "www.toaruos.org", strdup("23.235.47.133"));
 	hashmap_set(dns_cache, "www.yelp.com", strdup("104.16.57.23"));
 	hashmap_set(dns_cache, "s3-media2.fl.yelpcdn.com", strdup("199.27.79.175"));
+	hashmap_set(dns_cache, "forum.osdev.org", strdup("173.255.206.39"));
 
 	/* /dev/net/{domain|ip}/{protocol}/{port} */
 	vfs_mount("/dev/net", netfs_create());
