6cd1a40dbc1b915fe211898cf0d306bfe958eec7
Kevin Lange <kevin.lange@dakko.us>
2015-08-24 21:34:24 -0700

    Expose random as urandom as well

--- modules/random.c
+++ modules/random.c
@@ -61,6 +61,7 @@ static fs_node_t * random_device_create(void) {
 
 static int random_initialize(void) {
 	vfs_mount("/dev/random", random_device_create());
+	vfs_mount("/dev/urandom", random_device_create());
 	return 0;
 }
 
