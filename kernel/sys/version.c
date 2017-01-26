7c6004b1a8b25cf867668b3f56d5acd7decd5429
Kevin Lange <klange@dakko.us>
2017-01-26 22:27:46 +0900

    Bump kernel (for several updates from today)

--- kernel/sys/version.c
+++ kernel/sys/version.c
@@ -18,7 +18,7 @@ char * __kernel_version_format = "%d.%d.%d-%s";
 /* Version numbers X.Y.Z */
 int    __kernel_version_major = 0;
 int    __kernel_version_minor = 99;
-int    __kernel_version_lower = 24;
+int    __kernel_version_lower = 25;
 
 /* Kernel build suffix, which doesn't necessarily
  * mean anything, but can be used to distinguish
