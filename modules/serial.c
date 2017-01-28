a4d857493a0ec6a65a45ca414b46de9abd461bc6
Kevin Lange <klange@dakko.us>
2017-01-28 23:34:32 +0900

    Support fswait in serial devices

--- modules/serial.c
+++ modules/serial.c
@@ -142,6 +142,13 @@ static void close_serial(fs_node_t * node) {
 	return;
 }
 
+static int wait_serial(fs_node_t * node, void * process) {
+	return selectwait_fs(*pipe_for_port((int)node->device), process);
+}
+static int check_serial(fs_node_t * node) {
+	return selectcheck_fs(*pipe_for_port((int)node->device));
+}
+
 static fs_node_t * serial_device_create(int device) {
 	fs_node_t * fnode = malloc(sizeof(fs_node_t));
 	memset(fnode, 0x00, sizeof(fs_node_t));
@@ -158,6 +165,9 @@ static fs_node_t * serial_device_create(int device) {
 	fnode->readdir = NULL;
 	fnode->finddir = NULL;
 	fnode->ioctl   = NULL; /* TODO ioctls for raw serial devices */
+
+	fnode->selectcheck = check_serial;
+	fnode->selectwait  = wait_serial;
 
 	fnode->atime = now();
 	fnode->mtime = fnode->atime;
--- userspace/extra/serial-console.c
+++ userspace/extra/serial-console.c
@@ -18,28 +18,11 @@
 #include <fcntl.h>
 #include <sys/wait.h>
 
-#include "lib/pthread.h"
-
 int fd = 0;
-
-int child_pid = 0;
 
 int keep_echo = 0;
 int dos_lines = 0;
 int keep_canon = 0;
-
-void *print_serial_stuff(void * garbage) {
-	child_pid = gettid();
-
-	while (1) {
-		char buf[1024];
-		size_t r = read(fd, buf, 1024);
-		fwrite(buf, 1, r, stdout);
-		fflush(stdout);
-	}
-
-	pthread_exit(garbage);
-}
 
 struct termios old;
 
@@ -74,8 +57,6 @@ int show_usage(int argc, char * argv[]) {
 }
 
 int main(int argc, char ** argv) {
-	pthread_t receive_thread;
-	pthread_t flush_thread;
 
 	int arg = 1;
 	char * device;
@@ -106,49 +87,56 @@ int main(int argc, char ** argv) {
 
 	fd = open(device, 0, 0);
 
-	pthread_create(&receive_thread, NULL, print_serial_stuff, NULL);
+	int fds[2] = {STDIN_FILENO, fd};
 
 	while (1) {
-		char c = fgetc(stdin);
-		if (c == 0x1D) { /* ^] */
-			while (1) {
-				printf("serial-console> ");
-				set_buffered();
-				fflush(stdout);
+		int index = syscall_fswait(2, fds);
 
-				char line[1024];
-				fgets(line, 1024, stdin);
+		if (index == -1) {
+			fprintf(stderr, "serial-console: fswait: erroneous file descriptor\n");
+			fprintf(stderr, "serial-console: (did you try to open a file that isn't a serial console?\n");
+			return 1;
+		}
 
-				if (feof(stdin)) {
-					kill(child_pid, SIGKILL);
-					printf("Waiting for threads to shut down...\n");
-					while (wait(NULL) != -1);
-					printf("Exiting.\n");
-					return 0;
-				}
-
-				int i = strlen(line);
-				line[i-1] = '\0';
-
-				if (!strcmp(line, "quit")) {
-					kill(child_pid, SIGKILL);
-					printf("Waiting for threads to shut down...\n");
-					while (wait(NULL) != -1);
-					printf("Exiting.\n");
-					return 0;
-				} else if (!strcmp(line, "continue")) {
-					set_unbuffered();
+		if (index == 0) {
+			char c = fgetc(stdin);
+			if (c == 0x1D) { /* ^] */
+				while (1) {
+					printf("serial-console> ");
+					set_buffered();
 					fflush(stdout);
-					break;
+
+					char line[1024];
+					fgets(line, 1024, stdin);
+
+					if (feof(stdin)) {
+						return 0;
+					}
+
+					int i = strlen(line);
+					line[i-1] = '\0';
+
+					if (!strcmp(line, "quit")) {
+						return 0;
+					} else if (!strcmp(line, "continue")) {
+						set_unbuffered();
+						fflush(stdout);
+						break;
+					}
 				}
-			}
-		} else {
-			if (dos_lines && c == '\n') {
-				char buf[1] = {'\r'};
+			} else {
+				if (dos_lines && c == '\n') {
+					char buf[1] = {'\r'};
+					write(fd, buf, 1);
+				}
+				char buf[1] = {c};
 				write(fd, buf, 1);
 			}
-			char buf[1] = {c};
-			write(fd, buf, 1);
+		} else {
+			char buf[1024];
+			size_t r = read(fd, buf, 1024);
+			fwrite(buf, 1, r, stdout);
+			fflush(stdout);
 		}
 	}
 
