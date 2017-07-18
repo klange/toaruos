ea5546d1804e86ae225e592f3b46f66353154a42
Kevin Lange <klange@dakko.us>
2017-07-18 21:52:16 +0900

    ugh

--- util/build-travis.sh
+++ util/build-travis.sh
@@ -15,6 +15,31 @@ i686-pc-toaru-gcc --version
 # Cheating and copying /usr/python from toolchain
 cp -r /opt/toaruos/hdd/usr/python hdd/usr/python
 
+pushd hdd/usr
+    if [ ! -d bin ]; then
+        mkdir bin
+    fi
+
+    if [ ! -d lib]; then
+        mkdir lib
+    fi
+
+    pushd bin
+
+        # Can never be too careful.
+        ln -s ../python/bin/python3.6 python3.6
+        ln -s ../python/bin/python3.6 python3
+        ln -s ../python/bin/python3.6 python
+
+    popd
+
+    pushd lib
+
+        ln -s ../python/lib/libpython3.6m.so
+
+    popd
+popd
+
 # Build the CD
 make cdrom
 
