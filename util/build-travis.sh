52b9ba9d83c05907aa3747bc20028e9f3b1d6677
Kevin Lange <klange@dakko.us>
2017-07-18 21:36:09 +0900

    Use the toolchain's Python instead of building a fresh one

--- util/build-travis.sh
+++ util/build-travis.sh
@@ -12,22 +12,10 @@ env
 # Print cross GCC version for reference
 i686-pc-toaru-gcc --version
 
-# Clone the toolchain's Python checkout
-git clone /opt/toaruos/toaru-python toaru-python
+# Cheating and copying /usr/python from toolchain
+cp -r /opt/toaruos/hdd/usr/python hdd/usr/python
 
-# Do the first build pass
-make cdrom # Base pass to build libs
-
-# Build the Python stuff
-echo "travis_fold:start:Python"
-toolchain/install-python.sh
-echo "travis_fold:end:Python"
-
-echo "travis_fold:start:PyCairo"
-toolchain/install-pycairo.sh
-echo "travis_fold:end:PyCairo"
-
-# Do a final build
-make cdrom # Build with python/pycairo
+# Build the CD
+make cdrom
 
 # We're done!
