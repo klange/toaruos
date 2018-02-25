5965459a2c79b38f19f379ce5bf4dcc0628e758b
Mike Gerow <gerow@mgerow.com>
2018-02-25 07:40:58 +0000

    Upgrade toochain gcc from 6.3.0 to 6.4.0
    This is mainly to address a failure to build from source with gcc 7 (which is
    the default gcc on debian buster) unless -fpermissive is set. It turns out gcc
    7's error here was very much a real mistake in gcc 6, which is why it got fixed
    in 6.4.0. I'd guess there are other such cases that 6.4.0 addresses that I
    didn't have the privilege to discover.
    
    Thankfully the current 6.3.0 patches apply cleanly (with zero fuzz) on 6.4.0,
    so this commit mainly just moves some names around.
    
    The failure message in question:
    
    g++ -fno-PIE -c   -g -O2 -DIN_GCC  -DCROSS_DIRECTORY_STRUCTURE   -fno-exceptions -fno-rtti -fasynchronous-unwind-tables -W -Wall -Wno-narrowing -Wwrite-strings -Wcast-qual -Wmissing-format-attribute -Woverloaded-virtual -pedantic -Wno-long-long -Wno-variadic-macros -Wno-overlength-strings   -DHAVE_CONFIG_H -I. -I. -I/home/gerow/repos/toaruos/toolchain/tarballs/gcc-6.3.0/gcc -I/home/gerow/repos/toaruos/toolchain/tarballs/gcc-6.3.0/gcc/. -I/home/gerow/repos/toaruos/toolchain/tarballs/gcc-6.3.0/gcc/../include -I/home/gerow/repos/toaruos/toolchain/tarballs/gcc-6.3.0/gcc/../libcpp/include  -I/home/gerow/repos/toaruos/toolchain/tarballs/gcc-6.3.0/gcc/../libdecnumber -I/home/gerow/repos/toaruos/toolchain/tarballs/gcc-6.3.0/gcc/../libdecnumber/dpd -I../libdecnumber -I/home/gerow/repos/toaruos/toolchain/tarballs/gcc-6.3.0/gcc/../libbacktrace   -o sanopt.o -MT sanopt.o -MMD -MP -MF ./.deps/sanopt.TPo /home/gerow/repos/toaruos/toolchain/tarballs/gcc-6.3.0/gcc/sanopt.c
    /home/gerow/repos/toaruos/toolchain/tarballs/gcc-6.3.0/gcc/ubsan.c: In function ‘bool ubsan_use_new_style_p(location_t)’:
    /home/gerow/repos/toaruos/toolchain/tarballs/gcc-6.3.0/gcc/ubsan.c:1474:23: error: ISO C++ forbids comparison between pointer and integer [-fpermissive]
           || xloc.file == '\0' || xloc.file[0] == '\xff'
                           ^~~~
    Makefile:1085: recipe for target 'ubsan.o' failed
    make[2]: *** [ubsan.o] Error 1

--- toolchain/prepare.sh
+++ toolchain/prepare.sh
@@ -14,7 +14,7 @@ pushd "$DIR" > /dev/null
     fi
     pushd tarballs > /dev/null
         $INFO "wget" "Pulling source packages..."
-        grab "gcc"  "http://www.netgull.com/gcc/releases/gcc-6.3.0" "gcc-6.3.0.tar.gz" || bail
+        grab "gcc"  "http://www.netgull.com/gcc/releases/gcc-6.4.0" "gcc-6.4.0.tar.gz" || bail
         #grab "mpc"  "http://www.multiprecision.org/mpc/download" "mpc-0.9.tar.gz"
         #grab "mpfr" "http://www.mpfr.org/mpfr-3.0.1" "mpfr-3.0.1.tar.gz"
         #grab "gmp"  "ftp://ftp.gmplib.org/pub/gmp-5.0.1" "gmp-5.0.1.tar.gz"
@@ -28,9 +28,9 @@ pushd "$DIR" > /dev/null
         grab "ncurses" "http://b.dakko.us/~klange/mirrors" "ncurses-5.9.tar.gz" || bail
         grab "vim" "ftp://ftp.vim.org/pub/vim/unix" "vim-7.3.tar.bz2" || bail
         $INFO "wget" "Pulled source packages."
-        rm -rf "binutils-2.27" "freetype-2.4.9" "gcc-6.3.0" "gmp-5.0.1" "libpng-1.5.13" "mpc-0.9" "mpfr-3.0.1" "newlib-1.19.0" "zlib-1.2.7" "pixman-0.28.2" "ncurses-5.9" "vim73"
+        rm -rf "binutils-2.27" "freetype-2.4.9" "gcc-6.4.0" "gmp-5.0.1" "libpng-1.5.13" "mpc-0.9" "mpfr-3.0.1" "newlib-1.19.0" "zlib-1.2.7" "pixman-0.28.2" "ncurses-5.9" "vim73"
         $INFO "tar"  "Decompressing..."
-        deco "gcc"  "gcc-6.3.0.tar.gz" || bail
+        deco "gcc"  "gcc-6.4.0.tar.gz" || bail
         #deco "mpc"  "mpc-0.9.tar.gz"
         #deco "mpfr" "mpfr-3.0.1.tar.gz"
         #deco "gmp"  "gmp-5.0.1.tar.gz"
@@ -45,7 +45,7 @@ pushd "$DIR" > /dev/null
         deco "vim" "vim-7.3.tar.bz2" || bail
         $INFO "tar"  "Decompressed source packages."
         $INFO "patch" "Patching..."
-        patc "gcc"  "gcc-6.3.0" || bail
+        patc "gcc"  "gcc-6.4.0" || bail
         #patc "mpc"  "mpc-0.9"
         #patc "mpfr" "mpfr-3.0.1"
         #patc "gmp"  "gmp-5.0.1"
