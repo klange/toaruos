d2f1449a4600a79e43f821276dd07ea77a753dc0
K. Lange <klange@toaruos.org>
2018-02-25 19:07:56 +0900

    fixup hello

--- hello.c
+++ hello.c
@@ -1,17 +1,6 @@
-#include <unistd.h>
-
-#include "lib/graphics.h"
+#include <stdio.h>
 
 int main(int argc, char * argv[]) {
-	gfx_context_t * gfx = init_graphics_fullscreen();
-	draw_fill(gfx, rgb(0xFF, 0x44, 0x22));
-
-	char * args[] = {
-		"/bin/sh",
-		NULL,
-	};
-
-	execvp("/bin/sh",args);
-
+	puts("Hello, world.");
 	return 0;
 }
