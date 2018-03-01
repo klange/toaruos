5ace6fce2aaa5ef6989b3d5616386f245e971aec
K. Lange <klange@toaruos.org>
2018-03-01 23:15:49 +0900

    print a message on assert failure

--- libc/assert.c
+++ libc/assert.c
@@ -1,4 +1,6 @@
+#include <stdio.h>
 
 void __assert_func(const char * file, int line, const char * func, const char * failedexpr) {
+ fprintf(stderr, "Assertion failed in %s:%d (%s): %s\n", file, line, func, failedexpr);
  // void
 }
