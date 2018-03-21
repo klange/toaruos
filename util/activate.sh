f100f47d8430c8f637b54ae41b12b28df8530215
Kevin Lange <kevin.lange@mujin.co.jp>
2018-03-21 17:17:48 +0900

    Fixes

--- util/activate.sh
+++ util/activate.sh
@@ -1,17 +1,6 @@
 #!/bin/bash
 
 DIR="$( cd "$( dirname "${BASH_SOURCE[0]}" )" && pwd )"
-
-if [ ! -e "$DIR/local/bin/i686-pc-toaru-gcc" ]; then
-    echo -n "Toolchain has not been built. Would you like to build it now? (y/n) " >&2
-    read response
-    case $response in
-        [yY]) bash $DIR/build-gcc.sh ;;
-        [nN]) exit 1 ;;
-        *) exit 1 ;;
-    esac
-fi
-
 export PATH="$DIR/local/bin:$PATH"
 export TOOLCHAIN="$DIR"
 echo "$DIR/local/bin:$PATH"
