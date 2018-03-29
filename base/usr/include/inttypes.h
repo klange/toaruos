08420f73873e013d67bb0a0dd1f82689f671921f
Kevin Lange <klange@dakko.us>
2018-03-29 22:43:38 +0900

    super silly clip implementation

---
+++ base/usr/include/inttypes.h
@@ -0,0 +1,4 @@
+#pragma once
+
+#include <stdint.h>
+#include <stddef.h>
---
+++ base/usr/include/locale.h
@@ -0,0 +1,14 @@
+#pragma once
+
+#warning Need locale.h
+
+#define LC_ALL      0
+#define LC_COLLATE  1
+#define LC_CTYPE    2
+#define LC_MONETARY 3
+#define LC_NUMERIC  4
+#define LC_TIME     5
+#define LC_MESSAGES 6
+
+
+#define setlocale(...) (NULL)
