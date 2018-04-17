a7b41bd6cc679cf14b784b176ce81169b12fce6f
K. Lange <klange@toaruos.org>
2018-04-17 23:15:20 +0900

    add write-out

---
+++ apps/write-out.c
@@ -0,0 +1,15 @@
+#include <unistd.h>
+#include <fcntl.h>
+#include <stdio.h>
+
+int main(int argc, char * argv[]) {
+	int fd = open(argv[1], O_WRONLY | O_CREAT);
+
+	while (1) {
+		char buf[1024];
+		size_t r = read(0, buf, 1024);
+		if (r == 0) break;
+		write(fd, buf, r);
+	}
+
+}
