da909e4f79db634446292ab7b96a2d2d6e9dde6f
K. Lange <klange@toaruos.org>
2018-05-08 23:04:49 +0900

    Add confreader library

---
+++ apps/test-conf.c
@@ -0,0 +1,18 @@
+#include <assert.h>
+#include <stdio.h>
+#include <string.h>
+#include <toaru/confreader.h>
+
+
+int main(int argc, char * argv[]) {
+	confreader_t * conf = confreader_load("/etc/demo.conf");
+
+	fprintf(stderr, "test 1\n");
+	assert(confreader_get(conf, "", "test") != NULL);
+	assert(!strcmp(confreader_get(conf, "", "test"),"hello"));
+
+	fprintf(stderr, "test 2\n");
+	assert(!strcmp(confreader_get(conf,"sec","tion"),"test"));
+
+	return 0;
+}
