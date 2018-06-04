91fdce2c27ab38e3992ec262370761f72f9d891e
K. Lange <klange@toaruos.org>
2018-06-04 15:24:53 +0900

    Fix const warning in libc/symlink

--- libc/unistd/symlink.c
+++ libc/unistd/symlink.c
@@ -4,7 +4,7 @@
 #include <syscall.h>
 #include <syscall_nums.h>
 
-DEFN_SYSCALL2(symlink, SYS_SYMLINK, char *, char *);
+DEFN_SYSCALL2(symlink, SYS_SYMLINK, const char *, const char *);
 
 int symlink(const char *target, const char *name) {
 	int r = syscall_symlink(target, name);
