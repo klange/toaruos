fdcd64e0712b54300db9ccf80b3d888a097bf980
K. Lange <klange@toaruos.org>
2018-07-17 16:54:42 +0900

    add script to run qemu directly with -kernel

---
+++ util/qemu.sh
@@ -0,0 +1,35 @@
+#!/bin/bash
+
+qemu-system-x86_64 \
+-kernel fatbase/kernel \
+-initrd \
+fatbase/mod/zero.ko,\
+fatbase/mod/random.ko,\
+fatbase/mod/serial.ko,\
+fatbase/mod/debug_sh.ko,\
+fatbase/mod/procfs.ko,\
+fatbase/mod/tmpfs.ko,\
+fatbase/mod/ata.ko,\
+fatbase/mod/ext2.ko,\
+fatbase/mod/iso9660.ko,\
+fatbase/mod/ps2kbd.ko,\
+fatbase/mod/ps2mouse.ko,\
+fatbase/mod/lfbvideo.ko,\
+fatbase/mod/vbox.ko,\
+fatbase/mod/vmware.ko,\
+fatbase/mod/vidset.ko,\
+fatbase/mod/packetfs.ko,\
+fatbase/mod/snd.ko,\
+fatbase/mod/ac97.ko,\
+fatbase/mod/net.ko,\
+fatbase/mod/pcnet.ko,\
+fatbase/mod/rtl.ko,\
+fatbase/mod/e1000.ko,\
+fatbase/mod/pcspkr.ko,\
+fatbase/ramdisk.img \
+-append "vid=qemu,,1440,,900 root=/dev/ram0,,nocache start=--migrate _start=--headless" \
+-nographic \
+-enable-kvm \
+-serial mon:stdio \
+-m 1G -soundhw ac97,pcspk
+
