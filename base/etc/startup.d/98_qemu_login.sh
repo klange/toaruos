c5c0e5b89c8d11f46403eb4713d5b8a59c2fdabc
K. Lange <klange@toaruos.org>
2018-08-13 18:07:19 +0900

    change the way 98_qemu_login works

--- base/etc/startup.d/98_qemu_login.sh
+++ base/etc/startup.d/98_qemu_login.sh
@@ -1,7 +1,8 @@
 #!/bin/sh
 
-export-cmd QEMU_USER qemu-fwcfg opt/org.toaruos.forceuser
+if qemu-fwcfg -q opt/org.toaruos.forceuser then true else exit 0
 
-if empty? "$QEMU_USER" then true else /bin/getty -a "$QEMU_USER"
-if empty? "$QEMU_USER" then true else reboot
+export-cmd QEMU_USER qemu-fwcfg opt/org.toaruos.forceuser
+/bin/getty -a "$QEMU_USER"
+reboot
 
