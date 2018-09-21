7bea4608666c0935b4e4193850cf86f201b63704
K. Lange <klange@toaruos.org>
2018-09-21 15:24:01 +0900

    Attempt to automatically calculate a reasonable ramdisk size

--- util/calc-size.sh
+++ util/calc-size.sh
@@ -2,11 +2,16 @@
 
 DIR="$( cd "$( dirname "${BASH_SOURCE[0]}" )" && pwd )"
 
-if [ -e "$DIR/../base/usr/share/fonts" ]; then
-  echo 20000
-elif [ -e "$DIR/../base/usr/python" ]; then
-  echo 11000
-else
-  echo 4096
-fi
+SPACE_REQ=$(du -sb "$DIR/../base" | cut -f 1)
+
+let "SIZE = $SPACE_REQ / 4096 + 2048"
+echo $SIZE
+
+#if [ -e "$DIR/../base/usr/share/fonts" ]; then
+#  echo 20000
+#elif [ -e "$DIR/../base/usr/python" ]; then
+#  echo 11000
+#else
+#  echo 4096
+#fi
 
