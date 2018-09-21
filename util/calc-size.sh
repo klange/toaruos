9dbfa0fe8732e1cfdf4193ea7e8d54133de857ac
K. Lange <klange@toaruos.org>
2018-09-21 20:32:17 +0900

    Build in a bit of overestimate to handle inode tables and stuff

--- util/calc-size.sh
+++ util/calc-size.sh
@@ -4,14 +4,6 @@ DIR="$( cd "$( dirname "${BASH_SOURCE[0]}" )" && pwd )"
 
 SPACE_REQ=$(du -sb "$DIR/../base" | cut -f 1)
 
-let "SIZE = $SPACE_REQ / 4096 + 2048"
+let "SIZE = ($SPACE_REQ / 3500)"
 echo $SIZE
-
-#if [ -e "$DIR/../base/usr/share/fonts" ]; then
-#  echo 20000
-#elif [ -e "$DIR/../base/usr/python" ]; then
-#  echo 11000
-#else
-#  echo 4096
-#fi
 
