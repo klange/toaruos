a5a186290abea86f1ea9a2c09ae6cdea7ca2e56d
K. Lange <klange@toaruos.org>
2018-10-02 21:52:51 +0900

    Make env more normal

--- apps/env.c
+++ apps/env.c
@@ -3,19 +3,46 @@
  * of the NCSA / University of Illinois License - see LICENSE.md
  * Copyright (C) 2013-2018 K. Lange
  *
- * env - Print environment
- *
- * Prints all the environment values.
+ * env - Print or set environment
  */
 #include <stdio.h>
+#include <stdlib.h>
 #include <unistd.h>
+#include <string.h>
+#include <errno.h>
+
+extern int _environ_size;
 
 int main(int argc, char ** argv) {
-	char ** env = environ;
+	int start = 1;
 
-	while (*env) {
-		printf("%s\n", *env);
-		env++;
+	if (start < argc && !strcmp(argv[start],"-i")) {
+		for (int i = 0; i < _environ_size; ++i) {
+			environ[i] = NULL;
+		}
+		start++;
+	}
+
+	for (; start < argc; ++start) {
+		if (!strchr(argv[start],'=')) {
+			break;
+		} else {
+			putenv(argv[start]);
+		}
+	}
+
+	if (start < argc) {
+		/* Execute command */
+		if (execvp(argv[start], &argv[start])) {
+			fprintf(stderr, "%s: %s: %s\n", argv[0], argv[start], strerror(errno));
+		}
+	} else {
+		char ** env = environ;
+
+		while (*env) {
+			printf("%s\n", *env);
+			env++;
+		}
 	}
 
 	return 0;
