399a1f2e0c03f7769a0f69fc275458b62499a22d
K. Lange <klange@toaruos.org>
2018-10-12 15:53:54 +0900

    libc: qsort: bail early when sizes are 0

--- libc/stdlib/qsort.c
+++ libc/stdlib/qsort.c
@@ -1,9 +1,15 @@
+#include <stdio.h>
 #include <stdlib.h>
 #include <stddef.h>
 #include <string.h>
 #include <sys/types.h>
 
+extern char * _argv_0;
+extern int __libc_debug;
+
 void qsort(void * base, size_t nmemb, size_t size, int (*compar)(const void *, const void *)) {
+	if (!nmemb) return;
+	if (!size) return;
 	for (size_t i = 0; i < nmemb-1; ++i) {
 		for (size_t j = 0; j < nmemb-1; ++j) {
 			void * left = (char *)base + size * j;
