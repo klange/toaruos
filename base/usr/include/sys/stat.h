fb0327a1cf4e8c4d2c8de37b20723e7eb63a0b46
K. Lange <klange@toaruos.org>
2018-10-12 12:45:39 +0900

    libc: some stat things should be signed

--- base/usr/include/sys/stat.h
+++ base/usr/include/sys/stat.h
@@ -16,10 +16,10 @@ struct stat  {
 	uint16_t  st_uid;
 	uint16_t  st_gid;
 	uint16_t  st_rdev;
-	uint32_t  st_size;
+	int32_t  st_size;
 	uint32_t  st_atime;
 	uint32_t  __unused1;
-	uint32_t  st_mtime;
+	int32_t  st_mtime;
 	uint32_t  __unused2;
 	uint32_t  st_ctime;
 	uint32_t  __unused3;
