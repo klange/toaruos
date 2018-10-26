5f89d992580d727447f1fcfb56499ad87338e5dd
K. Lange <klange@toaruos.org>
2018-10-26 10:17:54 +0900

    v1.7.0

--- kernel/sys/version.c
+++ kernel/sys/version.c
@@ -17,8 +17,8 @@ char * __kernel_version_format = "%d.%d.%d-%s";
 
 /* Version numbers X.Y.Z */
 int    __kernel_version_major = 1;
-int    __kernel_version_minor = 6;
-int    __kernel_version_lower = 1;
+int    __kernel_version_minor = 7;
+int    __kernel_version_lower = 0;
 
 /* Kernel build suffix, which doesn't necessarily
  * mean anything, but can be used to distinguish
