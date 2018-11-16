39307c34f716eb7f2e1c2e066960099c0b6e7c88
K. Lange <klange@toaruos.org>
2018-11-16 14:34:58 +0900

    travis: new build script with uid juggling

--- util/build-travis.sh
+++ util/build-travis.sh
@@ -1,5 +1,21 @@
 #!/bin/bash
 
+# Give other users access to /root
+# (We probably should have just built the build tools somewhere else...)
+chmod o+x /root
+
+# Who owns this directory?
+NEWUID=`stat -c '%u' .`
+
+# Create a fake user with this name
+useradd -u $NEWUID local
+
+# Map the build tools
 ln -s /root/gcc_local util/local
 
-make -j4
+# Run make as local
+runuser -u local -- make -j4
+
+# Remove the build tools
+rm util/local
+
