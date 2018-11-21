07e074f82dc2d343392177c32458e9e6cd8a3fe5
K. Lange <klange@toaruos.org>
2018-11-22 08:29:50 +0900

    init: quiet set USER and HOME for root in startup scripts...

--- base/etc/startup.d/99_runstart.sh
+++ base/etc/startup.d/99_runstart.sh
@@ -2,6 +2,10 @@
 
 export-cmd START kcmdline -g start
 
+# We haven't actually hit a login yet, so make sure these are set here...
+export USER=root
+export HOME=/home/root
+
 if equals? "$START" "--vga" then exec /bin/terminal-vga -l
 if equals? "$START" "--headless" then exec /bin/getty
 if empty? "$START" then exec /bin/compositor else exec /bin/compositor $START
