41b5e640bfca5091f8eb64404aec4c1499586afc
K. Lange <klange@toaruos.org>
2018-11-22 16:27:07 +0900

    libc: add mkstemp

--- libc/stdlib/mktemp.c
+++ libc/stdlib/mktemp.c
@@ -3,6 +3,7 @@
 #include <string.h>
 #include <unistd.h>
 #include <errno.h>
+#include <fcntl.h>
 
 char * mktemp(char * template) {
 	if (strstr(template + strlen(template)-6, "XXXXXX") != template + strlen(template) - 6) {
@@ -14,4 +15,9 @@ char * mktemp(char * template) {
 	sprintf(tmp,"%04d%02d", getpid(), _i++);
 	memcpy(template + strlen(template) - 6, tmp, 6);
 	return template;
+}
+
+int mkstemp(char * template) {
+	mktemp(template);
+	return open(template, O_RDWR | O_CREAT, 0600);
 }
