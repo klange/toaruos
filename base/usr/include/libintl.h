5b8d7e47c2d21bdbb2d368c6b368a4c5419d4c43
K. Lange <klange@toaruos.org>
2018-12-10 18:39:42 +0900

    libintl: ngettext headers

--- base/usr/include/libintl.h
+++ base/usr/include/libintl.h
@@ -8,4 +8,9 @@ extern char * gettext (const char * msgid);
 extern char * dgettext (const char * domainname, const char * msgid);
 extern char * dcgettext (const char * domainname, const char * msgid, int category);
 
+extern char * ngettext (const char * msgid, const char * msgid_plural, unsigned long int n);
+extern char * dngettext (const char * domainname, const char * msgid, const char * msgid_plural, unsigned long int n);
+extern char * dcngettext (const char * domainname, const char * msgid, const char * msgid_plural, unsigned long int n, int category);
+
+
 _End_C_Header
