79e9ea8a8a95dd40ed1055913c3e583607928c5b
zanyxdev <zanyxdev@gmail.com>
2019-08-08 09:32:20 +0300

    test access calls

--- apps/test_access.c
+++ apps/test_access.c
@@ -4,48 +4,38 @@
 #include <stdlib.h>
 #include <unistd.h>
 #include <fcntl.h>
-int main(int argc, char * argv[]) {
-    printf("\nUsage: %s [test_file]",argv[0]);
-    printf("\nFile %s:",argv[1]);
-    
-    int result;
+#include <errno.h>
 
-    result = access( argv[1], F_OK );
-    printf("\nAccess result F_OK: %d",result);
 
-    if( result != -1 ) {
-        printf("\nTest for existence - SUCCESS!");
-    } else {
-         printf("\nTest for existence - FAILLURE!");
-    }
+int main(int argc, char * argv[]) {    
+    if ( argc > 1 && argc <= 2 ) {
+        
+        char * filename = argv[1];
 
-    result = access( argv[1], X_OK );
-    printf("\nAccess result X_OK: %d",result);
-
-    if(  result != -1 ) {
-        printf("\nTest for execute or search permission - SUCCESS!");
-    } else {
-         printf("\nTest for execute or search permission - FAILLURE!");
-    }
-
-    result = access( argv[1], W_OK );
-    printf("\nAccess result W_OK: %d",result);
-
-    if(  result != -1 ) {
-        printf("\nTest for write permission - SUCCESS!");
-    } else {
-         printf("\nTest for write permission - FAILLURE!");
-    }
-    
-
-    result = access( argv[1], R_OK );
-    printf("\nAccess result R_OK: %d",result);
-
-    if(  result != -1 ) {
-        printf("\nTest for read permission - SUCCESS!");
-    } else {
-         printf("\nTest for read permission - FAILLURE!");
-    }
-    exit(0);
+        if (access(filename, F_OK) != 0){          
+            printf("ERROR %s: %s: %s\n", argv[0], argv[1], strerror(errno));
+        }
+        else {
+            if (access(filename, R_OK) == 0){
+                printf("You have read access to '%s'\n", filename);                
+            } else {
+                printf("ERROR %s: %s: %s\n", argv[0], argv[1], strerror(errno));
+            }
+            if (access(filename, W_OK) == 0){
+                printf("You have write access to '%s'\n", filename);
+            } else{
+                printf("ERROR %s: %s: %s\n", argv[0], argv[1], strerror(errno));
+            }
+            if (access(filename, X_OK) == 0){
+                printf("You have search access to '%s'\n", filename);
+            } else {
+                printf("ERROR %s: %s: %s\n", argv[0], argv[1], strerror(errno));
+            }
+        }
+    }else{
+        printf("\nUsage: %s [test_file]",argv[0]);
+        exit (1);
+    }      
+    exit(0);  
 }
 
