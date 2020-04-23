c6da6020f46c5375724112295296ce97d7e41e30
K. Lange <klange@toaruos.org>
2020-04-23 18:45:10 +0900

    png-test: remove

--- apps/png-test.c
+++
@@ -1,9 +0,0 @@
-#include <stdio.h>
-
-#include <toaru/graphics.h>
-#include <toaru/png.h>
-
-int main(int argc, char * argv[]) {
-	sprite_t sprite;
-	return load_sprite_png(&sprite,"/opt/logo_login.png");
-}
