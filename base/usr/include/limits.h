b72a8c7643e991b517c3191031c6e9f536b63639
K. Lange <klange@toaruos.org>
2021-06-05 18:51:56 +0900

    base: define some byte order macros

--- base/usr/include/limits.h
+++ base/usr/include/limits.h
@@ -1,3 +1,5 @@
 #pragma once
 
+#define _LITTLE_ENDIAN
+
 /* dummy */
--- base/usr/include/sys/param.h
+++ base/usr/include/sys/param.h
@@ -4,3 +4,11 @@
 #include <stddef.h>
 #include <limits.h>
 #include <signal.h>
+
+#define __LITTLE_ENDIAN 1234
+#define __BIG_ENDIAN    4321
+#define __BYTE_ORDER    __LITLE_ENDIAN
+
+#define BIG_ENDIAN __BIG_ENDIAN
+#define LITTLE_ENDIAN __LITTLE_ENDIAN
+#define BYTE_ORDER __BYTE_ORDER
