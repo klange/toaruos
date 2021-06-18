0a9dc8d7a2349ad2da3a3844b19dd03818cb8969
K. Lange <klange@toaruos.org>
2021-06-18 19:57:59 +0900

    libc: __assert_func should exit

--- libc/assert/assert.c
+++ libc/assert/assert.c
@@ -1,6 +1,7 @@
 #include <stdio.h>
+#include <stdlib.h>
 
 void __assert_func(const char * file, int line, const char * func, const char * failedexpr) {
  fprintf(stderr, "Assertion failed in %s:%d (%s): %s\n", file, line, func, failedexpr);
- // void
+ exit(1);
 }
