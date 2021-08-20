b216e05f74703b1f74f8e8d171f195795cbe69da
K. Lange <klange@toaruos.org>
2021-08-21 08:04:25 +0900

    dhclient: don't segfault on RO filesystem trying to write resolv.conf

--- apps/dhclient.c
+++ apps/dhclient.c
@@ -389,8 +389,10 @@ static int configure_interface(const char * if_name) {
 					ip_ntoa(ntohl(ip_data), addr);
 					printf("%s: %s: nameserver %s\n", _argv_0, if_name, addr);
 					FILE * resolve = fopen("/etc/resolv.conf","w");
-					fprintf(resolve, "nameserver %s\n", addr);
-					fclose(resolve);
+					if (resolve) {
+						fprintf(resolve, "nameserver %s\n", addr);
+						fclose(resolve);
+					} /* else, read-only file system? */
 				}
 				opt += len;
 			}
