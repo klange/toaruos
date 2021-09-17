60b3446f00316703f78fbb7a2a22dbbf2c9f7587
K. Lange <klange@toaruos.org>
2021-09-17 21:43:07 +0900

    e1000: add ref link

--- modules/e1000.c
+++ modules/e1000.c
@@ -6,6 +6,8 @@
  * This file is part of ToaruOS and is released under the terms
  * of the NCSA / University of Illinois License - see LICENSE.md
  * Copyright (C) 2017-2021 K. Lange
+ *
+ * @ref https://www.intel.com/content/dam/www/public/us/en/documents/manuals/pcie-gbe-controllers-open-source-manual.pdf
  */
 #include <kernel/types.h>
 #include <kernel/string.h>
