a697f1abf5afda340bd9adffe08a7b17d9b34127
K. Lange <klange@toaruos.org>
2021-09-24 00:12:29 +0900

    test-sigsegv: Add a deeper segfault to test debugging libs

--- apps/test-sigsegv.c
+++ apps/test-sigsegv.c
@@ -1,4 +1,11 @@
+#include <stdio.h>
+#include <string.h>
+
 int main(int argc, char * argv[]) {
-	*(volatile int*)0x12345 = 42;
+	if (argc > 1) {
+		fprintf(stderr, "%s\n", (char*)(uintptr_t)strtoul(argv[1],NULL,0));
+	} else {
+		*(volatile int*)0x12345 = 42;
+	}
 	return 0;
 }
