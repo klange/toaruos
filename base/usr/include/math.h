e8a1ffab461e393a407942b4a684ee5facd5acad
K. Lange <klange@toaruos.org>
2021-10-07 07:24:39 +0900

    libm: Add missing M_, INFINITY, NAN constants

--- base/usr/include/math.h
+++ base/usr/include/math.h
@@ -4,7 +4,10 @@
 
 _Begin_C_Header
 
-#define M_PI 3.1415926
+#define M_PI 3.14159265358979323846
+#define M_E  2.7182818284590452354
+#define NAN (__builtin_nanf(""))
+#define INFINITY (__builtin_inff())
 
 extern double floor(double x);
 extern int abs(int j);
