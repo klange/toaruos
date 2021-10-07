46c59e878642bccddb9ae69f06b0f0dbfff348b0
K. Lange <klange@toaruos.org>
2021-10-07 20:22:10 +0900

    base: Load iso9660 and add a desktop icon when mounting CD at boot

--- base/etc/startup.d/05_mountcd.sh
+++ base/etc/startup.d/05_mountcd.sh
@@ -4,4 +4,11 @@ if not stat -Lq /dev/cdrom0 then exit 0
 
 echo -n "Mounting CD..." > /dev/pex/splash
 
+insmod /mod/iso9660.ko
 mount iso /dev/cdrom0 /cdrom
+
+# Does it look like it might be ours?
+if not stat -Lq /cdrom/bootcat then exit 0
+
+echo -e "icon=cd\nrun=cd /cdrom ; exec file-browser\ntitle=CD-ROM" > /home/local/Desktop/5_cdrom.launcher
+
