ecfe0764cc5eaa8c65b4b463aff3ce26aaa69840
stevanson <87704926+stevanson@users.noreply.github.com>
2021-10-25 17:40:57 +0530

    create toaruosfetch.c
    toaruosfetch to get to know about the operating system
    author: @stevanson

---
+++ apps/toaruosfetch.c
@@ -0,0 +1,15 @@
+/* includes */
+#include <stdio.h>
+
+int main() {
+
+  /* banner + github */
+  const char banner[] = "toaruos\n";
+  const char github[] = "https://github.com/klange/toaruos\n";
+  
+  /* printing out */
+  printf(banner);
+  printf(github);
+  
+  return 0;
+}
