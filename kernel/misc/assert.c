4b62e4ab22c31eba2963856f402a9c2c2e48d9a1
K. Lange <klange@toaruos.org>
2021-10-25 18:24:49 +0900

    assert: use dprintf, not printf, when printing assertion message

--- kernel/misc/assert.c
+++ kernel/misc/assert.c
@@ -8,7 +8,7 @@
 
 void __assert_failed(const char * file, int line, const char * func, const char * cond) {
 	arch_fatal_prepare();
-	printf("%s:%d (%s) Assertion failed: %s\n", file, line, func, cond);
+	dprintf("%s:%d (%s) Assertion failed: %s\n", file, line, func, cond);
 	arch_dump_traceback();
 	arch_fatal();
 }
