f0409ff120417e426fda26793358332ea2be60c6
K. Lange <klange@toaruos.org>
2022-01-31 21:06:17 +0900

    aarch64: couple of less bad math functions in libc

--- libc/arch/aarch64/bad.c
+++ libc/arch/aarch64/bad.c
@@ -1,7 +1,9 @@
 /* bad math */
+#include <math.h>
 
 double sqrt(double x) {
-	return 0.0;
+	asm volatile ("fsqrt %d0, %d1" : "=w"(x) : "w"(x));
+	return x;
 }
 
 double atan2(double y, double x) {
@@ -13,5 +15,14 @@ double pow(double x, double y) {
 }
 
 double fmod(double x, double y) {
-	return 0.0;
+	int _x = fpclassify(x);
+	int _y = fpclassify(y);
+
+	if (_y == FP_NAN) return NAN;
+	if (_x == FP_INFINITE) return NAN;
+	if (_y == FP_ZERO) return NAN;
+	if (_x == FP_ZERO) return x;
+
+	long div = x / y;
+	return x - (double)div * y;
 }
