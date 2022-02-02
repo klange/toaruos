07e97e2784b36b4b6b9c2fa5dede55c0d016c1b6
K. Lange <klange@toaruos.org>
2022-02-03 08:15:02 +0900

    aarch64: A better memcpy for arm

--- kernel/misc/string.c
+++ kernel/misc/string.c
@@ -26,11 +26,27 @@ unsigned short * memsetw(unsigned short * dest, unsigned short val, int count) {
 
 #if 1
 void * memcpy(void * restrict dest, const void * restrict src, size_t n) {
-	char * d = dest;
-	const char * s = src;
+	uint64_t * d_64 = dest;
+	const uint64_t * s_64 = src;
+
+	for (; n >= 8; n -= 8) {
+		*d_64++ = *s_64++;
+	}
+
+	uint32_t * d_32 = (void*)d_64;
+	const uint32_t * s_32 = (const void*)s_64;
+
+	for (; n >= 4; n -= 4) {
+		*d_32++ = *s_32++;
+	}
+
+	uint8_t * d = (void*)d_32;
+	const uint8_t * s = (const void*)s_32;
+
 	for (; n > 0; n--) {
 		*d++ = *s++;
 	}
+
 	return dest;
 }
 #else
