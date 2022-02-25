2bce2aa40e7debb0da581508cb08ad568cdefb7c
K. Lange <klange@toaruos.org>
2022-02-25 21:36:21 +0900

    mkdisk: FAT overhead is too high again... overestimate more

--- util/mkdisk.sh
+++ util/mkdisk.sh
@@ -9,7 +9,7 @@ OUTDIR=`dirname $1`
 
 # Calculate required space
 SPACE_REQ=$(du -sb "$DIR/../fatbase" | cut -f 1)
-let "SIZE = ($SPACE_REQ / 1000000)"
+let "SIZE = ($SPACE_REQ / 950000)"
 
 # Minimum size
 if [ $SIZE -lt 14 ]; then
