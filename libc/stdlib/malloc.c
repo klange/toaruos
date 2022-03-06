992d805f2529dbaa33c2e2ccd28b7e861aa40774
K. Lange <klange@toaruos.org>
2022-03-06 15:58:59 +0900

    libc: use munmap sysfunc in free() for 'big bins'

--- libc/stdlib/malloc.c
+++ libc/stdlib/malloc.c
@@ -269,8 +269,6 @@ typedef struct _klmalloc_big_bin_header {
 	void * head;
 	uintptr_t size;
 	uint32_t bin_magic;
-	struct _klmalloc_big_bin_header * prev;
-	struct _klmalloc_big_bin_header * forward[SKIP_MAX_LEVEL+1];
 } klmalloc_big_bin_header;
 
 
@@ -285,11 +283,6 @@ typedef struct _klmalloc_bin_header_head {
  * Array of available bins.
  */
 static klmalloc_bin_header_head klmalloc_bin_head[NUM_BINS - 1];	/* Small bins */
-static struct _klmalloc_big_bins {
-	klmalloc_big_bin_header head;
-	int level;
-} klmalloc_big_bins;
-static klmalloc_big_bin_header * klmalloc_newest_big = NULL;		/* Newest big bin */
 
 /* }}} Bin management */
 /* Doubly-Linked List {{{ */
@@ -329,222 +322,6 @@ static inline klmalloc_bin_header * __attribute__ ((always_inline)) klmalloc_lis
 }
 
 /* }}} Lists */
-/* Skip List {{{ */
-
-/*
- * Skip lists are efficient
- * data structures for storing
- * and searching ordered data.
- *
- * Here, the skip lists are used
- * to keep track of big bins.
- */
-
-/*
- * Generate a random value in an appropriate range.
- * This is a xor-shift RNG.
- */
-static uint32_t __attribute__ ((pure)) klmalloc_skip_rand(void) {
-	static uint32_t x = 123456789;
-	static uint32_t y = 362436069;
-	static uint32_t z = 521288629;
-	static uint32_t w = 88675123;
-
-	uint32_t t;
-
-	t = x ^ (x << 11);
-	x = y; y = z; z = w;
-	return w = w ^ (w >> 19) ^ t ^ (t >> 8);
-}
-
-/*
- * Generate a random level for a skip node
- */
-static inline int __attribute__ ((pure, always_inline)) klmalloc_random_level(void) {
-	int level = 0;
-	/*
-	 * Keep trying to check rand() against 50% of its maximum.
-	 * This provides 50%, 25%, 12.5%, etc. chance for each level.
-	 */
-	while (klmalloc_skip_rand() < SKIP_P && level < SKIP_MAX_LEVEL) {
-		++level;
-	}
-	return level;
-}
-
-/*
- * Find best fit for a given value.
- */
-static klmalloc_big_bin_header * klmalloc_skip_list_findbest(uintptr_t search_size) {
-	klmalloc_big_bin_header * node = &klmalloc_big_bins.head;
-	/*
-	 * Loop through the skip list until we hit something > our search value.
-	 */
-	int i;
-	for (i = klmalloc_big_bins.level; i >= 0; --i) {
-		while (node->forward[i] && (node->forward[i]->size < search_size)) {
-			node = node->forward[i];
-			if (node)
-				assert((node->size + sizeof(klmalloc_big_bin_header)) % PAGE_SIZE == 0);
-		}
-	}
-	/*
-	 * This value will either be NULL (we found nothing)
-	 * or a node (we found a minimum fit).
-	 */
-	node = node->forward[0];
-	if (node) {
-		assert((uintptr_t)node % PAGE_SIZE == 0);
-		assert((node->size + sizeof(klmalloc_big_bin_header)) % PAGE_SIZE == 0);
-	}
-	return node;
-}
-
-/*
- * Insert a header into the skip list.
- */
-static void klmalloc_skip_list_insert(klmalloc_big_bin_header * value) {
-	/*
-	 * You better be giving me something valid to insert,
-	 * or I will slit your ****ing throat.
-	 */
-	assert(value != NULL);
-	assert(value->head != NULL);
-	assert((uintptr_t)value->head > (uintptr_t)value);
-	if (value->size > NUM_BINS) {
-		assert((uintptr_t)value->head < (uintptr_t)value + value->size);
-	} else {
-		assert((uintptr_t)value->head < (uintptr_t)value + PAGE_SIZE);
-	}
-	assert((uintptr_t)value % PAGE_SIZE == 0);
-	assert((value->size + sizeof(klmalloc_big_bin_header)) % PAGE_SIZE == 0);
-	assert(value->size != 0);
-
-	/*
-	 * Starting from the head node of the bin locator...
-	 */
-	klmalloc_big_bin_header * node = &klmalloc_big_bins.head;
-	klmalloc_big_bin_header * update[SKIP_MAX_LEVEL + 1];
-
-	/*
-	 * Loop through the skiplist to find the right place
-	 * to insert the node (where ->forward[] > value)
-	 */
-	int i;
-	for (i = klmalloc_big_bins.level; i >= 0; --i) {
-		while (node->forward[i] && node->forward[i]->size < value->size) {
-			node = node->forward[i];
-			if (node)
-				assert((node->size + sizeof(klmalloc_big_bin_header)) % PAGE_SIZE == 0);
-		}
-		update[i] = node;
-	}
-	node = node->forward[0];
-
-	/*
-	 * Make the new skip node and update
-	 * the forward values.
-	 */
-	if (node != value) {
-		int level = klmalloc_random_level();
-		/*
-		 * Get all of the nodes before this.
-		 */
-		if (level > klmalloc_big_bins.level) {
-			for (i = klmalloc_big_bins.level + 1; i <= level; ++i) {
-				update[i] = &klmalloc_big_bins.head;
-			}
-			klmalloc_big_bins.level = level;
-		}
-
-		/*
-		 * Make the new node.
-		 */
-		node = value;
-
-		/*
-		 * Run through and point the preceeding nodes
-		 * for each level to the new node.
-		 */
-		for (i = 0; i <= level; ++i) {
-			node->forward[i] = update[i]->forward[i];
-			if (node->forward[i])
-				assert((node->forward[i]->size + sizeof(klmalloc_big_bin_header)) % PAGE_SIZE == 0);
-			update[i]->forward[i] = node;
-		}
-	}
-}
-
-/*
- * Delete a header from the skip list.
- * Be sure you didn't change the size, or we won't be able to find it.
- */
-static void klmalloc_skip_list_delete(klmalloc_big_bin_header * value) {
-	/*
-	 * Debug assertions
-	 */
-	assert(value != NULL);
-	assert(value->head);
-	assert((uintptr_t)value->head > (uintptr_t)value);
-	if (value->size > NUM_BINS) {
-		assert((uintptr_t)value->head < (uintptr_t)value + value->size);
-	} else {
-		assert((uintptr_t)value->head < (uintptr_t)value + PAGE_SIZE);
-	}
-
-	/*
-	 * Starting from the bin header, again...
-	 */
-	klmalloc_big_bin_header * node = &klmalloc_big_bins.head;
-	klmalloc_big_bin_header * update[SKIP_MAX_LEVEL + 1];
-
-	/*
-	 * Find the node.
-	 */
-	int i;
-	for (i = klmalloc_big_bins.level; i >= 0; --i) {
-		while (node->forward[i] && node->forward[i]->size < value->size) {
-			node = node->forward[i];
-			if (node)
-				assert((node->size + sizeof(klmalloc_big_bin_header)) % PAGE_SIZE == 0);
-		}
-		update[i] = node;
-	}
-	node = node->forward[0];
-	while (node != value) {
-		node = node->forward[0];
-	}
-
-	if (node != value) {
-		node = klmalloc_big_bins.head.forward[0];
-		while (node->forward[0] && node->forward[0] != value) {
-			node = node->forward[0];
-		}
-		node = node->forward[0];
-	}
-	/*
-	 * If we found the node, delete it;
-	 * otherwise, we do nothing.
-	 */
-	if (node == value) {
-		for (i = 0; i <= klmalloc_big_bins.level; ++i) {
-			if (update[i]->forward[i] != node) {
-				break;
-			}
-			update[i]->forward[i] = node->forward[i];
-			if (update[i]->forward[i]) {
-				assert((uintptr_t)(update[i]->forward[i]) % PAGE_SIZE == 0);
-				assert((update[i]->forward[i]->size + sizeof(klmalloc_big_bin_header)) % PAGE_SIZE == 0);
-			}
-		}
-
-		while (klmalloc_big_bins.level > 0 && klmalloc_big_bins.head.forward[klmalloc_big_bins.level] == NULL) {
-			--klmalloc_big_bins.level;
-		}
-	}
-}
-
-/* }}} */
 /* Stack {{{ */
 /*
  * Pop an item from a block.
@@ -676,91 +453,22 @@ static void * __attribute__ ((malloc)) klmalloc(uintptr_t size) {
 		return item;
 	} else {
 		/*
-		 * Big bins.
+		 * Round requested size to a set of pages, plus the header size.
 		 */
-		klmalloc_big_bin_header * bin_header = klmalloc_skip_list_findbest(size);
-		if (bin_header) {
-			assert(bin_header->size >= size);
-			/*
-			 * If we found one, delete it from the skip list
-			 */
-			klmalloc_skip_list_delete(bin_header);
-			/*
-			 * Retreive the head of the block.
-			 */
-			uintptr_t ** item = klmalloc_stack_pop((klmalloc_bin_header *)bin_header);
-#if 0
-			/*
-			 * Resize block, if necessary
-			 */
-			assert(bin_header->head == NULL);
-			uintptr_t old_size = bin_header->size;
-			//uintptr_t rsize = size;
-			/*
-			 * Round the requeste size to our full required size.
-			 */
-			size = ((size + sizeof(klmalloc_big_bin_header)) / PAGE_SIZE + 1) * PAGE_SIZE - sizeof(klmalloc_big_bin_header);
-			assert((size + sizeof(klmalloc_big_bin_header)) % PAGE_SIZE == 0);
-			if (bin_header->size > size * 2) {
-				assert(old_size != size);
-				/*
-				 * If we have extra space, start splitting.
-				 */
-				bin_header->size = size;
-				assert(sbrk(0) >= bin_header->size + (uintptr_t)bin_header);
-				/*
-				 * Make a new block at the end of the needed space.
-				 */
-				klmalloc_big_bin_header * header_new = (klmalloc_big_bin_header *)((uintptr_t)bin_header + sizeof(klmalloc_big_bin_header) + size);
-				assert((uintptr_t)header_new % PAGE_SIZE == 0);
-				memset(header_new, 0, sizeof(klmalloc_big_bin_header) + sizeof(void *));
-				header_new->prev = bin_header;
-				if (bin_header->next) {
-					bin_header->next->prev = header_new;
-				}
-				header_new->next = bin_header->next;
-				bin_header->next = header_new;
-				if (klmalloc_newest_big == bin_header) {
-					klmalloc_newest_big = header_new;
-				}
-				header_new->size = old_size - (size + sizeof(klmalloc_big_bin_header));
-				assert(((uintptr_t)header_new->size + sizeof(klmalloc_big_bin_header)) % PAGE_SIZE == 0);
-				fprintf(stderr, "Splitting %p [now %zx] at %p [%zx] from [%zx,%zx].\n", (void*)bin_header, bin_header->size, (void*)header_new, header_new->size, old_size, size);
-				/*
-				 * Free the new block.
-				 */
-				klfree((void *)((uintptr_t)header_new + sizeof(klmalloc_big_bin_header)));
-			}
-#endif
-			return item;
-		} else {
-			/*
-			 * Round requested size to a set of pages, plus the header size.
-			 */
-			uintptr_t pages = (size + sizeof(klmalloc_big_bin_header)) / PAGE_SIZE + 1;
-			bin_header = (klmalloc_big_bin_header*)sbrk(PAGE_SIZE * pages);
-			bin_header->bin_magic = BIN_MAGIC;
-			assert((uintptr_t)bin_header % PAGE_SIZE == 0);
-			/*
-			 * Give the header the remaining space.
-			 */
-			bin_header->size = pages * PAGE_SIZE - sizeof(klmalloc_big_bin_header);
-			assert((bin_header->size + sizeof(klmalloc_big_bin_header)) % PAGE_SIZE == 0);
-			/*
-			 * Link the block in physical memory.
-			 */
-			bin_header->prev = klmalloc_newest_big;
-			if (bin_header->prev) {
-				bin_header->prev->next = bin_header;
-			}
-			klmalloc_newest_big = bin_header;
-			bin_header->next = NULL;
-			/*
-			 * Return the head of the block.
-			 */
-			bin_header->head = NULL;
-			return (void*)((uintptr_t)bin_header + sizeof(klmalloc_big_bin_header));
-		}
+		uintptr_t pages = (size + sizeof(klmalloc_big_bin_header)) / PAGE_SIZE + 1;
+		klmalloc_big_bin_header * bin_header = (klmalloc_big_bin_header*)sbrk(PAGE_SIZE * pages);
+		bin_header->bin_magic = BIN_MAGIC;
+		assert((uintptr_t)bin_header % PAGE_SIZE == 0);
+		/*
+		 * Give the header the remaining space.
+		 */
+		bin_header->size = pages * PAGE_SIZE - sizeof(klmalloc_big_bin_header);
+		assert((bin_header->size + sizeof(klmalloc_big_bin_header)) % PAGE_SIZE == 0);
+		/*
+		 * Return the head of the block.
+		 */
+		bin_header->head = NULL;
+		return (void*)((uintptr_t)bin_header + sizeof(klmalloc_big_bin_header));
 	}
 }
 /* }}} */
@@ -807,86 +515,9 @@ static void klfree(void *ptr) {
 		assert(bheader);
 		assert(bheader->head == NULL);
 		assert((bheader->size + sizeof(klmalloc_big_bin_header)) % PAGE_SIZE == 0);
-		/*
-		 * Coalesce forward blocks into us.
-		 */
-#if 0
-		if (bheader != klmalloc_newest_big) {
-			/*
-			 * If we are not the newest big bin, there is most definitely
-			 * something in front of us that we can read.
-			 */
-			assert((bheader->size + sizeof(klmalloc_big_bin_header)) % PAGE_SIZE == 0);
-			klmalloc_big_bin_header * next = (void *)((uintptr_t)bheader + sizeof(klmalloc_big_bin_header) + bheader->size);
-			assert((uintptr_t)next % PAGE_SIZE == 0);
-			if (next == bheader->next && next->head) { //next->size > NUM_BINS && next->head) {
-				/*
-				 * If that something is an available big bin, we can
-				 * coalesce it into us to form one larger bin.
-				 */
 
-				uintptr_t old_size = bheader->size;
-
-				klmalloc_skip_list_delete(next);
-				bheader->size = (uintptr_t)bheader->size + (uintptr_t)sizeof(klmalloc_big_bin_header) + next->size;
-				assert((bheader->size + sizeof(klmalloc_big_bin_header))  % PAGE_SIZE == 0);
-
-				if (next == klmalloc_newest_big) {
-					/*
-					 * If the guy in front of us was the newest,
-					 * we are now the newest (as we are him).
-					 */
-					klmalloc_newest_big = bheader;
-				} else {
-					if (next->next) {
-						next->next->prev = bheader;
-					}
-				}
-				fprintf(stderr,"Coelesced (forwards)  %p [%zx] <- %p [%zx] = %zx\n", (void*)bheader, old_size, (void*)next, next->size, bheader->size);
-			}
-		}
-#endif
-		/*
-		 * Coalesce backwards
-		 */
-#if 0
-		if (bheader->prev && bheader->prev->head) {
-			/*
-			 * If there is something behind us, it is available, and there is nothing between
-			 * it and us, we can coalesce ourselves into it to form a big block.
-			 */
-			if ((uintptr_t)bheader->prev + (bheader->prev->size + sizeof(klmalloc_big_bin_header)) == (uintptr_t)bheader) {
-
-				uintptr_t old_size = bheader->prev->size;
-
-				klmalloc_skip_list_delete(bheader->prev);
-				bheader->prev->size = (uintptr_t)bheader->prev->size + (uintptr_t)bheader->size + sizeof(klmalloc_big_bin_header);
-				assert((bheader->prev->size + sizeof(klmalloc_big_bin_header))  % PAGE_SIZE == 0);
-				klmalloc_skip_list_insert(bheader->prev);
-				if (klmalloc_newest_big == bheader) {
-					klmalloc_newest_big = bheader->prev;
-				} else {
-					if (bheader->next) {
-						bheader->next->prev = bheader->prev;
-					}
-				}
-				fprintf(stderr,"Coelesced (backwards) %p [%zx] <- %p [%zx] = %zx\n", (void*)bheader->prev, old_size, (void*)bheader, bheader->size, bheader->size);
-				/*
-				 * If we coalesced backwards, we are done.
-				 */
-				return;
-			}
-		}
-#endif
-		/*
-		 * Push new space back into the stack.
-		 */
-		klmalloc_stack_push((klmalloc_bin_header *)bheader, (void *)((uintptr_t)bheader + sizeof(klmalloc_big_bin_header)));
-		assert(bheader->head != NULL);
-		/*
-		 * Insert the block into list of available slabs.
-		 */
-		klmalloc_skip_list_insert(bheader);
+		char * args[] = {(char*)header, (char*)(bheader->size + sizeof(klmalloc_big_bin_header))};
+		syscall_sysfunc(43, args);
 	} else {
 		/*
 		 * If the stack is empty, we are freeing
@@ -956,19 +587,7 @@ static void * __attribute__ ((malloc)) klrealloc(void *ptr, uintptr_t size) {
 		old_size = (1UL << (SMALLEST_BIN_LOG + old_size));
 	}
 
-	/*
-	 * (This will only happen for a big bin, mathematically speaking)
-	 * If we still have room in our bin for the additonal space,
-	 * we don't need to do anything.
-	 */
-	if (old_size >= size) {
-
-		/*
-		 * TODO: Break apart blocks here, which is far more important
-		 *       than breaking them up on allocations.
-		 */
-		return ptr;
-	}
+	if (old_size == size) return ptr;
 
 	/*
 	 * Reallocate more memory.
@@ -981,7 +600,7 @@ static void * __attribute__ ((malloc)) klrealloc(void *ptr, uintptr_t size) {
 		 * Be sure to only copy as much as was in
 		 * the old block.
 		 */
-		memcpy(newptr, ptr, old_size);
+		memcpy(newptr, ptr, (old_size < size) ? old_size : size);
 		klfree(ptr);
 		return newptr;
 	}
