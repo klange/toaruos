ce7cb5891359e47d616fee863ca86dea6e8666fe
K. Lange <klange@toaruos.org>
2022-03-07 18:07:47 +0900

    ifconfig: cleanup, add commands to set addresses

--- apps/ifconfig.c
+++ apps/ifconfig.c
@@ -13,14 +13,17 @@
  */
 #include <stdio.h>
 #include <stdint.h>
+#include <string.h>
 #include <dirent.h>
 #include <fcntl.h>
+#include <ctype.h>
 #include <unistd.h>
 #include <sys/stat.h>
 #include <sys/ioctl.h>
 #include <sys/socket.h>
 #include <net/if.h>
 #include <arpa/inet.h>
+#include <netinet/in.h>
 
 extern char * _argv_0;
 
@@ -63,10 +66,14 @@ static int print_human_readable_size(char * _out, size_t s) {
 	return sprintf(_out, "%d B", (int)s);
 }
 
-static int configure_interface(const char * if_name) {
+static int open_netdev(const char * if_name) {
 	char if_path[100];
 	snprintf(if_path, 100, "/dev/net/%s", if_name);
-	int netdev = open(if_path, O_RDWR);
+	return open(if_path, O_RDONLY);
+}
+
+static int print_interface(const char * if_name) {
+	int netdev = open_netdev(if_name);
 
 	if (netdev < 0) {
 		perror(_argv_0);
@@ -133,29 +140,113 @@ static int configure_interface(const char * if_name) {
 	return 0;
 }
 
-int main(int argc, char * argv[]) {
+static int print_all_interfaces(void) {
 	int retval = 0;
 
-	if (argc > 1) {
-		return configure_interface(argv[1]);
-	} else {
-		/* Read /dev/net for interfaces */
-		DIR * d = opendir("/dev/net");
-		if (!d) {
-			fprintf(stderr, "%s: no network?\n", _argv_0);
-			return 1;
-		}
-
-		struct dirent * ent;
-		while ((ent = readdir(d))) {
-			if (ent->d_name[0] == '.') continue;
-			if (configure_interface(ent->d_name)) {
-				retval = 1;
-			}
-		}
-
-		closedir(d);
+	/* Read /dev/net for interfaces */
+	DIR * d = opendir("/dev/net");
+	if (!d) {
+		fprintf(stderr, "%s: no network?\n", _argv_0);
+		return 1;
 	}
 
+	struct dirent * ent;
+	while ((ent = readdir(d))) {
+		if (ent->d_name[0] == '.') continue;
+
+		/* Retrieve data for the interface and print the results. */
+		if (print_interface(ent->d_name)) {
+			retval = 1;
+		}
+	}
+
+	closedir(d);
 	return retval;
+}
+
+static int maybe_address(const char * s) {
+	/* Our inet_addr is not great, let's help it a little... */
+	int dots = 0;
+	for (;*s;s++) {
+		if (*s == '.') {
+			dots++;
+			if (dots > 3) return 0;
+			continue;
+		}
+		if (!isdigit(*s)) return 0;
+	}
+
+	return dots == 3;
+}
+
+static int parse_address(const char * cmd, const char * addr, in_addr_t * out) {
+	if (!addr) {
+		fprintf(stderr, "%s: %s: expected argument\n", _argv_0, cmd);
+		return 1;
+	}
+	if (!maybe_address(addr)) {
+		fprintf(stderr, "%s: %s: '%s' doesn't look like a valid address\n", _argv_0, cmd, addr);
+		return 1;
+	}
+
+	*out = inet_addr(addr);
+	return 0;
+}
+
+static int _set_address(int netdev, const char * cmd, const char * arg, const char * ioctlstr, unsigned long ioctltype) {
+	int status;
+	in_addr_t ip;
+	if ((status = parse_address(cmd, arg, &ip))) return status;
+	if ((status = ioctl(netdev, ioctltype, &ip))) { perror(ioctlstr); return status; }
+	return 0;
+}
+
+#define set_address(cmd, arg, itype) _set_address(netdev, cmd, arg, #itype, itype)
+#define command_with_address(cmd, itype) if (!strcmp(argv[i], cmd)) { if (_set_address(netdev, argv[i], argv[i+1], #itype, itype)) { return 1; } continue; }
+
+int main(int argc, char * argv[]) {
+	/* Figure out what we're trying to do. */
+	if (argc < 2) return print_all_interfaces();
+
+	/* Handle (ignore) some common commands */
+	if (!strcmp(argv[1], "up") || !strcmp(argv[1],"down")) {
+		fprintf(stderr, "%s: 'up' and 'down' commands are unsupported\n", argv[0]);
+		return 1;
+	}
+
+	/* If there is an interface name and nothing else, print and be done with it. */
+	if (argc == 2) return print_interface(argv[1]);
+
+	/* All other options here require a leading interface. */
+	int netdev = open_netdev(argv[1]);
+
+	if (netdev < 0) {
+		perror(argv[0]);
+		return 1;
+	}
+
+	/* Now let's figure out what we want to do with remaining options */
+	int collected_address = 0;
+
+	for (int i = 2; i < argc; ++i) {
+		/* Is this argument an address? */
+		if (maybe_address(argv[i])) {
+			/* Try to set IPv4 address */
+			if (collected_address) {
+				fprintf(stderr, "%s: expected at most one bare address, but found a second\n", argv[0]);
+				return 1;
+			}
+			if (set_address("inet", argv[i], SIOCSIFADDR)) return 1;
+			collected_address = 1;
+		} else {
+			command_with_address("netmask", SIOCSIFNETMASK);
+			command_with_address("gw", SIOCSIFGATEWAY);
+			command_with_address("gateway", SIOCSIFGATEWAY);
+			command_with_address("inet", SIOCSIFADDR);
+			fprintf(stderr, "%s: '%s' is not an understood command\n", argv[0], argv[i]);
+			return 1;
+		}
+	}
+
+	return 0;
 }
