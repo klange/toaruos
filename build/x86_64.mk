2d52464c8f218d411e1ba3b01e6d7742137dfd41
K. Lange <klange@toaruos.org>
2022-03-16 08:39:10 +0900

    base: Don't use large pages when building libs for x86-64

--- build/x86_64.mk
+++ build/x86_64.mk
@@ -2,6 +2,7 @@ ARCH=x86_64
 
 ARCH_KERNEL_CFLAGS  = -mno-red-zone -fno-omit-frame-pointer -mfsgsbase
 ARCH_KERNEL_CFLAGS += -mgeneral-regs-only -z max-page-size=0x1000 -nostdlib
+ARCH_USER_CFLAGS += -z max-page-size=0x1000
 
 TARGET=x86_64-pc-toaru
 
