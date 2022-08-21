603a07a868330a90ea899448b619f87a31e3b8ae
K. Lange <klange@toaruos.org>
2022-08-22 08:44:45 +0900

    sh: that decoder is in a header...

--- apps/sh.c
+++ apps/sh.c
@@ -40,6 +40,7 @@
 #include <toaru/hashmap.h>
 #include <toaru/kbd.h>
 #include <toaru/rline.h>
+#include <toaru/decodeutf8.h>
 
 #ifndef environ
 extern char **environ;
@@ -173,46 +174,6 @@ void gethost() {
 
 	int len = strlen(buf.nodename);
 	memcpy(_hostname, buf.nodename, len+1);
-}
-
-#define UTF8_ACCEPT 0
-#define UTF8_REJECT 1
-static inline uint32_t decode(uint32_t* state, uint32_t* codep, uint32_t byte) {
-	static int state_table[32] = {
-		0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0, /* 0xxxxxxx */
-		1,1,1,1,1,1,1,1,                 /* 10xxxxxx */
-		2,2,2,2,                         /* 110xxxxx */
-		3,3,                             /* 1110xxxx */
-		4,                               /* 11110xxx */
-		1                                /* 11111xxx */
-	};
-
-	static int mask_bytes[32] = {
-		0x7F,0x7F,0x7F,0x7F,0x7F,0x7F,0x7F,0x7F,
-		0x7F,0x7F,0x7F,0x7F,0x7F,0x7F,0x7F,0x7F,
-		0x00,0x00,0x00,0x00,0x00,0x00,0x00,0x00,
-		0x1F,0x1F,0x1F,0x1F,
-		0x0F,0x0F,
-		0x07,
-		0x00
-	};
-
-	static int next[5] = {
-		0,
-		1,
-		0,
-		2,
-		3
-	};
-
-	if (*state == UTF8_ACCEPT) {
-		*codep = byte & mask_bytes[byte >> 3];
-		*state = state_table[byte >> 3];
-	} else if (*state > 0) {
-		*codep = (byte & 0x3F) | (*codep << 6);
-		*state = next[*state];
-	}
-	return *state;
 }
 
 int display_width_of_string(const char * str) {
