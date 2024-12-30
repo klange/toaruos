e03e2ff189c3d424701a2dd10dc7ffbd55a3f346
K. Lange <klange@toaruos.org>
2024-12-30 17:29:48 +0900

    ptrace: fixup x86-64 mmu_page_is_user_writable
    Fixes https://github.com/klange/toaruos/issues/303

--- base/usr/include/kernel/arch/x86_64/pml.h
+++ base/usr/include/kernel/arch/x86_64/pml.h
@@ -23,4 +23,4 @@ union PML {
 };
 
 #define mmu_page_is_user_readable(p) (p->bits.user)
-#define mmu_page_is_user_writable(p) (p->bits.writable)
+#define mmu_page_is_user_writable(p) (p->bits.user && p->bits.writable)
