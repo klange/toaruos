e42b25a515b24594140ebbf21d8a3a852cae4a15
K. Lange <klange@toaruos.org>
2025-09-02 12:22:15 +0900

    aarch64: move pl011 uart config out of tasklet
    If we do this in a kernel thread, other threads can run simultaneously
    with the disable/configure/enable cycle resulting in warning messages
    from QEMU. Instead, do it before starting the thread so it is more
    likely to be atomic.

--- kernel/arch/aarch64/pl011.c
+++ kernel/arch/aarch64/pl011.c
@@ -2,10 +2,13 @@
  * @file  kernel/arch/aarch64/pl011.c
  * @brief Rudimentary serial driver for the pl011 uart
  *
+ * TODO: Headers with sensible register names and macros for bits.
+ * TODO: TTY configuration support.
+ *
  * @copyright
  * This file is part of ToaruOS and is released under the terms
  * of the NCSA / University of Illinois License - see LICENSE.md
- * Copyright (C) 2022 K. Lange
+ * Copyright (C) 2022-2025 K. Lange
  */
 #include <stdint.h>
 #include <kernel/process.h>
@@ -53,9 +56,6 @@ static void pl011_thread(void * arg) {
 	gic_assign_interrupt(1, pl011_irq, (void*)uart_mapped);
 
 	/* Enable interrupts */
-	uart_mapped[12] = 0;
-	uart_mapped[11] = 0x70;
-	uart_mapped[12] = 0x301;
 	uart_mapped[14] |= (1 << 4);
 	asm volatile ("isb" ::: "memory");
 
@@ -77,8 +77,24 @@ void pl011_start(void) {
 	/* I know this is going to be 0x09000000, but let's find it anyway */
 	uint32_t * reg = dtb_node_find_property(uart, "reg");
 	uintptr_t  uart_base = swizzle(reg[3]);
-	void * uart_mapped = mmu_map_mmio_region(uart_base, 0x1000);
-	spawn_worker_thread(pl011_thread, "[pl011]", uart_mapped);
+	volatile uint32_t * uart_mapped = (volatile uint32_t*)mmu_map_mmio_region(uart_base, 0x1000);
+
+	/*
+	 * PL011 UART configuration
+	 *
+	 * Setup line control here so we do it atomically, otherwise doing it in the thread
+	 * can cause other cores printing debug messages to write when the port is disabled,
+	 * which causes QEMU to print warning messages.
+	 *
+	 * We are missing a few things because this is only intended to work with QEMU.
+	 * We should probably add support for baud rate configuration and actually map
+	 * the line control configuration to the TTY state.
+	 */
+	uart_mapped[12] = 0;     /* UARTCR  - Disable the UART */
+	uart_mapped[11] = 0x70;  /* UARTLCR - Stick parity disabled, 8-bit words, tx+rx FIFO enabled; one stop bit, (odd parity), parity disabled, no break */
+	uart_mapped[12] = 0x301; /* UARTCR  - Enable tx/rx, enable UART */
+
+	spawn_worker_thread(pl011_thread, "[pl011]", (void*)uart_mapped);
 }
 
 
