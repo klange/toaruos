80e2b0b38dcc7e4f8f808099ac541819d17e50f0
K. Lange <klange@toaruos.org>
2026-02-06 10:28:14 +0900

    strings: support reading from stdin

--- apps/strings.c
+++ apps/strings.c
@@ -13,10 +13,50 @@
 #include <unistd.h>
 #include <errno.h>
 
+static char format = 0;
+static int min_chars = 4;
+
+static void read_file(FILE * f) {
+	char buf[1024] = {0};
+	int  ind = 0;
+	size_t offset = 0;
+
+	while (!feof(f)) {
+		int c = fgetc(f);
+		if (c < 0) break;
+		if (c == '\n' || c == '\0') {
+			if (ind >= min_chars) {
+				switch (format) {
+					case 'x':
+						fprintf(stdout, "%lx ", offset - ind);
+						break;
+					case 'd':
+						fprintf(stdout, "%lu ", offset - ind);
+						break;
+					default:
+						break;
+				}
+				buf[ind] = '\0';
+				fprintf(stdout, "%s\n", buf);
+			}
+			ind = 0;
+			offset++;
+			continue;
+		}
+		if (!isprint(c)) {
+			ind = 0;
+		} else {
+			if (ind < 1024) {
+				buf[ind] = c;
+				ind++;
+			}
+		}
+		offset++;
+	}
+}
+
 int main(int argc, char * argv[]) {
 	int opt;
-	char format = 0;
-	int min_chars = 4;
 	int ret_val = 0;
 
 	while ((opt = getopt(argc, argv, "an:t:")) != -1) {
@@ -38,6 +78,9 @@ int main(int argc, char * argv[]) {
 		}
 	}
 
+	if (optind == argc) {
+		read_file(stdin);
+	}
 	for (int i = optind; i < argc; ++i) {
 		FILE * f = fopen(argv[i], "r");
 		if (!f) {
@@ -45,43 +88,7 @@ int main(int argc, char * argv[]) {
 			ret_val = 1;
 			continue;
 		}
-
-		char buf[1024] = {0};
-		int  ind = 0;
-		size_t offset = 0;
-
-		while (!feof(f)) {
-			int c = fgetc(f);
-			if (c < 0) break;
-			if (c == '\n' || c == '\0') {
-				if (ind >= min_chars) {
-					switch (format) {
-						case 'x':
-							fprintf(stdout, "%lx ", offset - ind);
-							break;
-						case 'd':
-							fprintf(stdout, "%lu ", offset - ind);
-							break;
-						default:
-							break;
-					}
-					buf[ind] = '\0';
-					fprintf(stdout, "%s\n", buf);
-				}
-				ind = 0;
-				offset++;
-				continue;
-			}
-			if (!isprint(c)) {
-				ind = 0;
-			} else {
-				if (ind < 1024) {
-					buf[ind] = c;
-					ind++;
-				}
-			}
-			offset++;
-		}
+		read_file(f);
 		fclose(f);
 	}
 
