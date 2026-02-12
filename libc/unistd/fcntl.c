a3e4446b0d379fe29ced0adebb82514ed0034a65
K. Lange <klange@toaruos.org>
2026-02-12 12:51:53 +0900

    libc: use system call for fcntl
    closes #339

--- libc/unistd/fcntl.c
+++ libc/unistd/fcntl.c
@@ -2,22 +2,27 @@
 #include <fcntl.h>
 #include <va_list.h>
 #include <stdint.h>
-#include <sys/sysfunc.h>
+#include <syscall.h>
+#include <syscall_nums.h>
+
+DEFN_SYSCALL3(fcntl, SYS_FCNTL, int, int, long);
 
 int fcntl(int fd, int cmd, ...) {
-    switch (cmd) {
-        case F_GETFD:
-            return 0;
-        case F_SETFD:
-            return 0;
-        case F_DUPFD: {
-            va_list ap;
-            va_start(ap, cmd);
-            int arg = va_arg(ap, int); /* "taken as an integer of type int" */
-            va_end(ap);
-            return sysfunc(TOARU_SYS_FUNC_FCNTL_DUPFD, (char*[]){(char*)(uintptr_t)fd,(char*)(uintptr_t)arg});
-        }
-    }
-    errno = EINVAL;
-    return -1;
+	va_list ap;
+	va_start(ap, cmd);
+	long arg = 0;
+	switch (cmd) {
+		case F_SETFD:
+		case F_SETFL:
+		case F_DUPFD:
+			arg = va_arg(ap, int); /* "taken as an integer of type int" */
+			break;
+		case F_GETLK:
+		case F_SETLK:
+		case F_SETLKW:
+			arg = (long)(uintptr_t)va_arg(ap, struct flock *);
+			break;
+	}
+	va_end(ap);
+	__sets_errno(syscall_fcntl(fd, cmd, arg));
 }
