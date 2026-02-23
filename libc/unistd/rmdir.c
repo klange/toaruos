8bc638838cd0813863630e98454304bd26e5d404
K. Lange <klange@toaruos.org>
2026-02-23 20:21:24 +0900

    libc: implement rmdir with lstat+unlink

--- libc/unistd/rmdir.c
+++ libc/unistd/rmdir.c
@@ -1,7 +1,18 @@
 #include <unistd.h>
 #include <errno.h>
+#include <sys/stat.h>
 
 int rmdir(const char *pathname) {
-	errno = ENOTSUP;
-	return -1;
+	/* XXX: This is subject to TOCTOU issues, but whatever. */
+	struct stat st;
+
+	/* pathname must directly name a directory, not a symlink */
+	if (lstat(pathname, &st) < 0) return -1;
+	if (!S_ISDIR(st.st_mode)) {
+		errno = ENOTDIR;
+		return -1;
+	}
+
+	/* our unlink can remove empty directories */
+	return unlink(pathname);
 }
