de0aeebe6cf4234d135cd1b4e6f5de663997fce7
K. Lange <klange@toaruos.org>
2026-03-02 08:37:41 +0900

    libc: execl* family; execvpe
    Also removes debug functionality that is obviated by strace
    
    closes #416

--- libc/unistd/execvp.c
+++ libc/unistd/execvp.c
@@ -1,6 +1,7 @@
 #include <stdio.h>
 #include <syscall.h>
 #include <syscall_nums.h>
+#include <stdarg.h>
 #include <stdlib.h>
 #include <string.h>
 #include <errno.h>
@@ -9,25 +10,14 @@
 DEFN_SYSCALL3(execve, SYS_EXECVE, char *, char **, char **);
 
 extern char ** environ;
-extern char * _argv_0;
-extern int __libc_debug;
 
 #define DEFAULT_PATH "/bin:/usr/bin"
 
 int execve(const char *name, char * const argv[], char * const envp[]) {
-	if (__libc_debug) {
-		fprintf(stderr, "%s: execve(\"%s\"", _argv_0, name);
-		char * const* arg = argv;
-		while (*arg) {
-			fprintf(stderr,", \"%s\"", *arg);
-			arg++;
-		}
-		fprintf(stderr, ")\n");
-	}
 	__sets_errno(syscall_execve((char*)name,(char**)argv,(char**)envp));
 }
 
-int execvp(const char *file, char *const argv[]) {
+int execvpe(const char *file, char *const argv[], char *const envp[]) {
 	if (file && (!strstr(file, "/"))) {
 		/* We don't quite understand "$PATH", so... */
 		char * path = getenv("PATH");
@@ -52,18 +42,84 @@ int execvp(const char *file, char *const argv[]) {
 				continue; /* XXX not technically correct; need to test perms */
 			}
 
-			return execve(exe, argv, environ);
+			return execve(exe, argv, envp);
 		}
 		free(xpath);
 		errno = ENOENT;
 		return -1;
 	} else if (file) {
-		return execve(file, argv, environ);
+		return execve(file, argv, envp);
 	}
 	errno = ENOENT;
 	return -1;
 }
 
+int execvp(const char *file, char *const argv[]) {
+	return execvpe(file, argv, environ);
+}
+
 int execv(const char * file, char * const argv[]) {
 	return execve(file, argv, environ);
+}
+
+int execl(const char *path, const char *arg, ...) {
+	int argc = 1; /* Count */
+	va_list ap;
+
+	/* Count */
+	va_start(ap, arg);
+	while (va_arg(ap, char *)) argc++;
+	va_end(ap);
+
+	/* Copy */
+	char * argv[argc+1];
+	va_start(ap, arg);
+	argv[0] = (char*)arg;
+	for (int i = 1; i <= argc; ++i) argv[i] = va_arg(ap, char*);
+	va_end(ap);
+
+	/* Exec */
+	return execv(path, argv);
+}
+
+int execlp(const char *path, const char *arg, ...) {
+	int argc = 1; /* Count */
+	va_list ap;
+
+	/* Count */
+	va_start(ap, arg);
+	while (va_arg(ap, char *)) argc++;
+	va_end(ap);
+
+	/* Copy */
+	char * argv[argc+1];
+	va_start(ap, arg);
+	argv[0] = (char*)arg;
+	for (int i = 1; i <= argc; ++i) argv[i] = va_arg(ap, char*);
+	va_end(ap);
+
+	/* Exec */
+	return execvp(path, argv);
+}
+
+int execle(const char *path, const char *arg, ...) {
+	int argc = 1; /* Count */
+	va_list ap;
+
+	/* Count */
+	va_start(ap, arg);
+	while (va_arg(ap, char *)) argc++;
+	va_end(ap);
+
+	/* Copy */
+	char * argv[argc+1];
+	va_start(ap, arg);
+	argv[0] = (char*)arg;
+	for (int i = 1; i <= argc; ++i) argv[i] = va_arg(ap, char*);
+
+	char ** envp = va_arg(ap, char**);
+	va_end(ap);
+
+	/* Exec */
+	return execve(path, argv, envp);
 }
