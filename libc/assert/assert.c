f257ad67344a004f2f8655d7f99ebcb7f2a66081
K. Lange <klange@toaruos.org>
2026-03-23 22:52:56 +0900

    libc: __assert_func() should abort()

--- libc/assert/assert.c
+++ libc/assert/assert.c
@@ -1,7 +1,8 @@
 #include <stdio.h>
 #include <stdlib.h>
+#include <signal.h>
 
 void __assert_func(const char * file, int line, const char * func, const char * failedexpr) {
  fprintf(stderr, "Assertion failed in %s:%d (%s): %s\n", file, line, func, failedexpr);
- exit(1);
+ abort();
 }
