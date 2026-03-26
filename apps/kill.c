cf4d7fe07a2fc478da79d4a44815f8f88e780cec
K. Lange <klange@toaruos.org>
2026-03-26 11:46:13 +0900

    kill: use sig2str, str2sig to implement a better kill

--- apps/kill.c
+++ apps/kill.c
@@ -15,114 +15,145 @@
 #include <signal.h>
 #include <ctype.h>
 #include <errno.h>
-
-struct sig_def {
-	int sig;
-	const char * name;
-};
-
-struct sig_def signals[] = {
-	{SIGHUP,"HUP"},
-	{SIGINT,"INT"},
-	{SIGQUIT,"QUIT"},
-	{SIGILL,"ILL"},
-	{SIGTRAP,"TRAP"},
-	{SIGABRT,"ABRT"},
-	{SIGEMT,"EMT"},
-	{SIGFPE,"FPE"},
-	{SIGKILL,"KILL"},
-	{SIGBUS,"BUS"},
-	{SIGSEGV,"SEGV"},
-	{SIGSYS,"SYS"},
-	{SIGPIPE,"PIPE"},
-	{SIGALRM,"ALRM"},
-	{SIGTERM,"TERM"},
-	{SIGUSR1,"USR1"},
-	{SIGUSR2,"USR2"},
-	{SIGCHLD,"CHLD"},
-	{SIGPWR,"PWR"},
-	{SIGWINCH,"WINCH"},
-	{SIGURG,"URG"},
-	{SIGPOLL,"POLL"},
-	{SIGSTOP,"STOP"},
-	{SIGTSTP,"TSTP"},
-	{SIGCONT,"CONT"},
-	{SIGTTIN,"TTIN"},
-	{SIGTTOUT,"TTOUT"},
-	{SIGVTALRM,"VTALRM"},
-	{SIGPROF,"PROF"},
-	{SIGXCPU,"XCPU"},
-	{SIGXFSZ,"XFSZ"},
-	{SIGWAITING,"WAITING"},
-	{SIGDIAF,"DIAF"},
-	{SIGHATE,"HATE"},
-	{SIGWINEVENT,"WINEVENT"},
-	{SIGCAT,"CAT"},
-	{0,NULL},
-};
+#include <sys/wait.h>
 
 int usage(char * argv[]) {
+#define X_S "\033[3m"
+#define X_E "\033[0m"
 	fprintf(stderr,
 			"%s - send a signal to another process\n"
 			"\n"
-			"usage: %s [-\033[3mx\033[0m] \033[3mprocess\033[0m\n"
+			"usage: %s [-" X_S "name" X_E "] " X_S "pid..." X_E "\n"
+			"       %s -l [" X_S "exit_status" X_E "]\n"
 			"\n"
-			" -h --help       \033[3mShow this help message.\033[0m\n"
-			" -\033[3mx\033[0m              \033[3mSignal number to send\033[0m\n"
-			" -\033[3mSIGNAME\033[0m        \033[3mSignal name to send\033[0m\n"
-			"\n",
-			argv[0], argv[0]);
+			" --help          " X_S "Show this help message." X_E "\n"
+			" -s " X_S "name         Specify the signal to send." X_E "\n"
+			" -l              " X_S "List available signals." X_E "\n"
+			" -l " X_S "exit_status  Interpret an exit status or signal number and print its name." X_E "\n"
+			" -" X_S "name           Same as -s name." X_E "\n"
+			"\n"
+			"Signals may be named with or without the SIG prefix, or numerically.\n",
+			argv[0], argv[0], argv[0]);
 	return 1;
 }
 
 int main(int argc, char * argv[]) {
-	int signum = SIGKILL;
-	int pid = 0;
-	int i = 1;
+	/* Special getopt-alike argument parsing */
 
-	if (argc < 2) return usage(argv);
+	int optind = 1;
+	char * signal_name = NULL;
 
-	if (argv[1][0] == '-') {
-		if (argv[1][1] == 'h' || !strcmp(argv[1],"--help")) {
-			return usage(argv);
-		}
-		signum = -1;
-		if (strlen(argv[1]+1) > 3 && strstr(argv[1]+1,"SIG") == (argv[1]+1)) {
-			struct sig_def * s = signals;
-			while (s->name) {
-				if (!strcmp(argv[1]+4,s->name)) {
-					signum = s->sig;
+	while (optind < argc) {
+		if (*argv[optind] == '-') {
+			switch (argv[optind][1]) {
+				case 'l':
+					if (signal_name) return usage(argv);
+					if (argv[optind][2] != '\0') {
+						signal_name = &argv[optind][1];
+						goto _next_opt;
+					}
+					/* Assume -l flag */
+					char signame[SIG2STR_MAX] = {0};
+					if (optind + 1 == argc) {
+						/* Print everything */
+						for (int i = 1; i < NSIG; ++i) {
+							if (!sig2str(i, signame)) {
+								fprintf(stdout, "%s%c", signame, i + 1 == NSIG ? '\n' : ' ');
+							}
+						}
+						return 0;
+					} else {
+						if (optind + 2 != argc) return usage(argv);
+						int status = atoi(argv[optind+1]);
+
+						if (status < NSIG) {
+							if (!sig2str(status, signame)) {
+								fprintf(stdout, "%s\n", signame);
+								return 0;
+							}
+						}
+
+						if (status > 128 && status - 128 < NSIG) {
+							if (!sig2str(status-128,signame)) {
+								fprintf(stdout, "%s\n", signame);
+								return 0;
+							}
+						}
+
+						return 1;
+					}
 					break;
-				}
-				s++;
+				case 's':
+					if (signal_name) return usage(argv);
+					if (argv[optind][2] != '\0') {
+						/* Assume signal name */
+						signal_name = &argv[optind][1];
+						goto _next_opt;
+					} else {
+						/* Signal must be next argument */
+						if (optind + 1 == argc) return usage(argv);
+						signal_name = argv[optind+1];
+						optind++; /* Argument */
+						goto _next_opt;
+					}
+					break;
+				case '0' ... '9':
+				case 'a' ... 'k':
+				case 'm' ... 'r':
+				case 't' ... 'z':
+				case 'A' ... 'Z':
+					if (signal_name) return usage(argv);
+					/* Signal name */
+					signal_name = &argv[optind][1];
+					goto _next_opt;
+				case '-':
+					/* End of arguments or long arguments */
+					if (!argv[optind][2]) {
+						optind++;
+						goto _end_opt;
+					} else if (!strcmp(&argv[optind][2],"help")) {
+						usage(argv);
+						return 0;
+					} else {
+						fprintf(stderr, "%s: '%s' is not a recognized long option.\n", argv[0], argv[optind]);
+						return usage(argv);
+					}
+				default:
+					return usage(argv);
 			}
 		} else {
-			if (!isdigit(argv[1][1])) {
-				struct sig_def * s = signals;
-				while (s->name) {
-					if (!strcmp(argv[1]+1,s->name)) {
-						signum = s->sig;
-						break;
-					}
-					s++;
-				}
-			} else {
-				signum = atoi(argv[1]+1);
-			}
+			break;
 		}
-		if (signum == -1) {
-			fprintf(stderr,"%s: %s: invalid signal specification\n",argv[0],argv[1]+1);
-			return 1;
-		}
-		i++;
+
+_next_opt:
+		optind++;
+		continue;
+_end_opt:
+		break;
 	}
 
-	if (i == argc) return usage(argv);
+	if (optind == argc) {
+		return usage(argv);
+	}
+
+	int signum = SIGKILL;
+	if (signal_name) {
+		/* Convert name to upper case */
+		for (char * s = signal_name; *s; ++s) {
+			*s = toupper(*s);
+		}
+
+		/* Get signal from str2sig */
+		if (str2sig(signal_name, &signum)) {
+			fprintf(stderr, "%s: '%s' is not an understood signal.\n", argv[0], signal_name);
+			return 1;
+		}
+	}
 
 	int retval = 0;
 
-	for (; i < argc; ++i) {
-		pid = atoi(argv[i]);
+	for (int i = optind; i < argc; ++i) {
+		int pid = atoi(argv[i]);
 		if (pid) {
 			if (kill(pid, signum) < 0) {
 				fprintf(stderr, "%s: (%d) %s\n", argv[0], pid, strerror(errno));
