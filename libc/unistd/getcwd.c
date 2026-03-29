3eefe4ced6b7b647f5b18cc0cd22bb3854c76b0d
K. Lange <klange@toaruos.org>
2026-03-29 16:49:13 +0900

    libc: getcwd support GNU extensions properly

--- libc/unistd/getcwd.c
+++ libc/unistd/getcwd.c
@@ -8,12 +8,28 @@
 DEFN_SYSCALL2(getcwd, SYS_GETCWD, char *, size_t);
 
 char *getcwd(char *buf, size_t size) {
-	if (!buf) buf = malloc(size);
-	long result = syscall_getcwd(buf, size);
+
+	/* If you want us to allocate exactly enough space, you can't provide a pointer. */
+	if (buf && !size) {
+		errno = -EINVAL;
+		return NULL;
+	}
+
+	char * path = buf;
+	size_t want = size;
+
+	if (!size) want = PATH_MAX;
+	if (!buf) path = malloc(want);
+
+	long result = syscall_getcwd(path, want);
+
+	if (result > 0 && !buf && !size) return realloc(path, result);
+
 	if (result < 0) {
 		errno = -result;
 		return NULL;
 	}
-	return buf;
+
+	return path;
 }
 
