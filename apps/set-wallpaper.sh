7c0f4b972dd755ce7b55b1056d669f7b54b2a677
K. Lange <klange@toaruos.org>
2026-04-06 08:46:21 +0900

    set-wallpaper.sh: improvements; add manpage

--- apps/set-wallpaper.sh
+++ apps/set-wallpaper.sh
@@ -1,11 +1,15 @@
 #!/bin/esh
 
+# Validate argument, make sure the image exists and can be loaded
 if empty? "$1" then exec sh -c "echo 'usage: $0 WALLPAPER'"
-if not stat -Lq "$1" then exec sh -c "echo '$0: $1 does not exist'"
+export-cmd NEW_WALLPAPER realpath "$1"
+if not stat -Lq "$NEW_WALLPAPER" then exec sh -c "echo '$0: $1: no such file or directory'"
+if not check-image -q "$NEW_WALLPAPER" then exec sh -c "echo '$0: $1: not a valid image'"
 
+# Write the wallpaper to the config
+echo "wallpaper=$NEW_WALLPAPER" > $HOME/.wallpaper.conf
+
+# Tell the desktop to reload it
 export-cmd DESKTOP cat /var/run/.wallpaper.pid
-if empty? "$DESKTOP" then sh -c "echo '$0: No wallpaper running?'"
-
-echo "wallpaper=$1" > $HOME/.wallpaper.conf
-
+if empty? "$DESKTOP" then exec sh -c "echo '$0: No wallpaper running?'"
 kill -SIGUSR1 $DESKTOP
