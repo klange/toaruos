d3436f68ebe6b0e3aad24a6c1ec1fbb98b6edb46
K. Lange <klange@toaruos.org>
2026-04-08 10:43:59 +0900

    man: add -k mode

--- apps/man.c
+++ apps/man.c
@@ -16,6 +16,7 @@
 #include <unistd.h>
 #include <string.h>
 #include <stdint.h>
+#include <dirent.h>
 #include <errno.h>
 #include <sys/wait.h>
 #include <sys/stat.h>
@@ -28,9 +29,13 @@
 #define TOARU_MAN_PATH_PREFIX ""
 #endif
 
-#define MAN_FMT  TOARU_MAN_PATH_PREFIX "/usr/share/man/man%s/%s.%s"
+#define MAN_DIR  TOARU_MAN_PATH_PREFIX "/usr/share/man/man%s"
+#define MAN_FMT  MAN_DIR "/%s.%s"
 #define ROFF_CMD TOARU_MAN_TOOL_PREFIX "roff"
 #define MORE_CMD TOARU_MAN_TOOL_PREFIX "more -rP'%s(%s)' --stay --alt"
+
+/* Don't bother prefixing this; I don't want to build our grep elsewhere. */
+#define GREP_CMD "grep --color -i -- '%s' -"
 
 static int where_is = 0;
 
@@ -38,7 +43,8 @@ static int usage(char * argv[]) {
 #define X_S "\033[3m"
 #define X_E "\033[0m"
 	fprintf(stderr,
-		"usage: %s [-a] [-S " X_S "sectionlist" X_E "] [" X_S "section" X_E "] " X_S "page" X_E "\n"
+		"usage: %s [-a] [-S " X_S "sectionlist" X_E "] [" X_S "section" X_E "] " X_S "page..." X_E "\n"
+		"       %s -k [-S " X_S "sectionlist" X_E "] " X_S "keyword..." X_E "\n"
 		"\n"
 		"Display a manual page.\n"
 		"\n"
@@ -47,15 +53,16 @@ static int usage(char * argv[]) {
 		" -a       " X_S "Display all matching manuals." X_E "\n"
 		" -w       " X_S "Print the locations of manual pages." X_E "\n"
 		" -S " X_S "list  Limit search to colon-separated list of sections." X_E "\n"
+		" -k       " X_S "Search manual page NAME sections for keywords." X_E "\n"
 		" --help   " X_S "Show this help text." X_E "\n"
-		"\n", argv[0]);
+		"\n", argv[0], argv[0]);
 	return 1;
 }
 
 /**
  * @brief Try to display a manual page.
  *
- * Checks for the existence of @c filename and, if found, possibly
+ * Checks for the existence of @p filename and, if found, possibly
  * decompresses it, passes it to @b roff, and pipes it to @b more.
  *
  * @param filename Full path to the file to check.
@@ -89,7 +96,7 @@ static int try_filename(char * filename, char * page, char * i) {
 /**
  * @brief Look for a page in a section.
  *
- * Checks whether @c page in section @c i exists
+ * Checks whether @p page in section @p i exists
  * at the configured location, with or without
  * a @b .gz suffix.
  *
@@ -112,6 +119,111 @@ static int try_section(char *i, char * page) {
 	}
 	free(filename);
 	return 0;
+}
+
+/**
+ * @brief Search pages in a section for a keyword.
+ *
+ * Looks at all the manual pages in section @p i and processes
+ * them through 'roff -S NAME -P | grep -i' to determine if
+ * they contain a requested @p keyword. First, this is done
+ * in a dry-run mode where the output is written to /dev/null.
+ * Then, if the page matched, its name is printed and it is
+ * processed again for actual output.
+ *
+ * This will only try to look at files in the section that
+ * actually look like pages, so if you ask for section 1
+ * and a file doesn't match '{page}.1' or '{page}.1.gz',
+ * it will be ignored. This matches the behavior of our page
+ * lookup elsewhere, as man will not find these pages if
+ * ask for them by name.
+ *
+ * We try to pass @p keyword to grep as a single argument,
+ * but nothing in this stack does input validation, so it's
+ * up to the user to not shell inject themselves with a bad
+ * keyword argument.
+ *
+ * Because we run 'roff' with the '-P' option, grep should
+ * be able to match across formatting changes correctly.
+ *
+ * Also we enable --color in grep because it looks nice.
+ *
+ * @param i Section to search in
+ * @param keyword Pattern to pass to grep
+ * @returns 1 if a page matched or 0 otherwise.
+ */
+static int search_section(char * i, char * keyword) {
+	size_t len_i = strlen(i);
+	char * dirpath;
+	asprintf(&dirpath, MAN_DIR, i);
+	DIR * dir = opendir(dirpath);
+	if (!dir) return 0; /* Not a valid section? */
+
+	int found = 0;
+	struct dirent * ent;
+	while ((ent = readdir(dir))) {
+		if (ent->d_name[0] == '.') continue;
+
+		char * filename;
+		asprintf(&filename,"%s/%s", dirpath, ent->d_name);
+		int is_gz = strlen(filename) > 3 && !strcmp(filename + strlen(filename)-3,".gz");
+
+		char * page = strdup(ent->d_name);
+		size_t len_page = strlen(page);
+
+		if (is_gz) {
+			/* Exclude .gz suffix from printed name */
+			page[len_page - 3] = '\0';
+			len_page -= 3;
+		}
+
+		if (len_page > len_i + 1 && page[len_page - len_i - 1] == '.' &&
+			!strcmp(page + len_page - len_i, i)) {
+			/* Exclude section suffix from printed name */
+			page[len_page - len_i - 1] = '\0';
+			len_page -= len_i + 1;
+		} else {
+			/* And reject files that don't look like man pages for this section. */
+			free(page);
+			free(filename);
+			continue;
+		}
+
+		int dry_run = 1;
+		while (dry_run >= 0) {
+
+			if (!dry_run) {
+				/* If not in dry run, prefix the output without the page and section
+				 * so the user can actually find it. Try to format this nicely. */
+				int written = fprintf(stdout, "%s (%s)", page, i);
+				while (written < 30) {
+					fprintf(stdout, " ");
+					written++;
+				}
+				fprintf(stdout, " - ");
+				fflush(stdout);
+			}
+
+			char * systemcmd;
+			asprintf(&systemcmd,
+				!is_gz ?
+				(ROFF_CMD " -S NAME -P '%s' | " GREP_CMD "%s") :
+				("gunzip -c '%s' | " ROFF_CMD " -S NAME -- - | " GREP_CMD "%s"),
+				filename, keyword, dry_run ? " >/dev/null" : "");
+			int result = system(systemcmd);
+			free(systemcmd);
+			if (result != 0) break;
+
+			found |= 1;
+			dry_run--;
+		}
+
+		free(page);
+		free(filename);
+	}
+	free(dirpath);
+	closedir(dir);
+	return found;
 }
 
 /**
@@ -201,8 +313,9 @@ int main(int argc, char * argv[]) {
 			"1:n:l:8:3:0:2:3krk:5:4:9:6:7";
 
 	int show_all = 0;
+	int search_names = 0;
 	int opt;
-	while ((opt = getopt(argc, argv, "?awS:-:")) != -1) {
+	while ((opt = getopt(argc, argv, "?awS:k-:")) != -1) {
 		switch (opt) {
 			case 'a':
 				show_all = 1;
@@ -212,6 +325,9 @@ int main(int argc, char * argv[]) {
 				break;
 			case 'S':
 				section_list = optarg;
+				break;
+			case 'k':
+				search_names = 1;
 				break;
 			case '-':
 				if (!strcmp(optarg,"help")) {
@@ -238,7 +354,7 @@ int main(int argc, char * argv[]) {
 
 	/* If there is more than one argument and it looks like it might be a section
 	 * then treat it is as such and use it for all of the rest of the arguments. */
-	if (optind + 1 != argc && is_section(argv[optind])) {
+	if (!search_names && optind + 1 != argc && is_section(argv[optind])) {
 		section = argv[optind];
 		optind++;
 	}
@@ -246,7 +362,11 @@ int main(int argc, char * argv[]) {
 	for (; optind != argc; ++optind) {
 		int found = 0;
 
-		if (is_file_name(argv[optind])) {
+		if (search_names) {
+			for (char ** s = section ? (char*[]){section, NULL} : sections; *s; ++s) {
+				found |= search_section(*s, argv[optind]);
+			}
+		} else if (is_file_name(argv[optind])) {
 			/* Accept things that look like raw paths to roff sources */
 			if (try_filename(argv[optind], argv[optind], "")) found = 1;
 		} else if (section) {
@@ -263,7 +383,11 @@ int main(int argc, char * argv[]) {
 		}
 
 		if (!found) {
-			fprintf(stderr, "No manual entry for %s\n", argv[optind]);
+			fprintf(stderr,
+				search_names
+					? "%s: nothing appropriate\n"
+					: "No manual entry for %s\n",
+				argv[optind]);
 			ret |= 1;
 		}
 	}
