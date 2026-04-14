f6ba6e94c74a661fd967e00dccf0062fd1d13658
K. Lange <klange@toaruos.org>
2026-04-14 13:33:26 +0900

    libc: add locking in atexit()

--- libc/stdlib/atexit.c
+++ libc/stdlib/atexit.c
@@ -1,19 +1,40 @@
 #include <stdlib.h>
 
-static void (*_atexit_handlers[32])(void) = {NULL};
-static int _atexit_count = 0;
+/* Header-only */
+#include <toaru/spinlock.h>
+
+/* Make these visible in case we want to poke them elsewhere.
+ * Should be fine since we use names that are reserved for us as the libc. */
+int volatile __atexit_lock = 0;
+void (*__atexit_handlers[32])(void) = {NULL};
+int __atexit_count = 0;
 
 void _handle_atexit(void) {
-	if (!_atexit_count) return;
-	do {
-		_atexit_count--;
-		_atexit_handlers[_atexit_count]();
-	} while (_atexit_count);
+	spin_lock(&__atexit_lock);
+	if (__atexit_count) {
+		/* atexit handlers can very well call atexit() themselves,
+		 * so we do this awkward loop to ensure we're picking up
+		 * any changes to the count and always running the
+		 * most recently added ones. */
+		do {
+			__atexit_count--;
+			void (*handler)(void) = __atexit_handlers[__atexit_count];
+			spin_unlock(&__atexit_lock);
+			handler();
+			spin_lock(&__atexit_lock);
+		} while (__atexit_count);
+	}
+	spin_unlock(&__atexit_lock);
 }
 
 int atexit(void (*h)(void)) {
-	if (_atexit_count == ATEXIT_MAX) return 1;
-	_atexit_handlers[_atexit_count++] = h;
+	spin_lock(&__atexit_lock);
+	if (__atexit_count == ATEXIT_MAX) {
+		spin_unlock(&__atexit_lock);
+		return -1; /* Spec says "non-zero", but maybe -1 is expected. */
+	}
+	__atexit_handlers[__atexit_count++] = h;
+	spin_unlock(&__atexit_lock);
 	return 0;
 }
 
