4057a39636d8c5e9388464f38e3c510de96b0149
K. Lange <klange@toaruos.org>
2026-04-18 21:43:44 +0900

    hostname: cleanup

--- apps/hostname.c
+++ apps/hostname.c
@@ -9,28 +9,41 @@
 #include <stdio.h>
 #include <unistd.h>
 #include <string.h>
+#include <errno.h>
+
+#define HOSTNAME_FILE "/etc/hostname"
+
+static int usage(char * argv[]) {
+	fprintf(stderr, "usage: %s [-fsd] [hostname]\n", argv[0]);
+	return 1;
+}
 
 int main(int argc, char * argv[]) {
-	if ((argc > 1 && argv[1][0] == '-') || (argc < 2)) {
-		char tmp[256] = {0};
-		gethostname(tmp, 255);
-		printf("%s\n", tmp);
-		return 0;
-	} else {
-		if (geteuid() != 0) {
-			fprintf(stderr,"Must be root to set hostname.\n");
-			return 1;
-		} else {
-			sethostname(argv[1], strlen(argv[1]));
-			FILE * file = fopen("/etc/hostname", "w");
-			if (!file) {
-				return 1;
-			} else {
-				fprintf(file, "%s\n", argv[1]);
-				fclose(file);
-				return 0;
-			}
+	int opt;
+	while ((opt = getopt(argc, argv, "dfs")) != -1) {
+		switch (opt) {
+			case 'd':
+			case 'f':
+			case 's':
+				break; /* ignore all of these */
+			case '?':
+				return usage(argv);
 		}
 	}
+
+	if (optind + 1 < argc) usage(argv);
+
+	if (optind < argc) {
+		if (sethostname(argv[optind], strlen(argv[optind])) == -1) return fprintf(stderr, "%s: sethostname: %s\n", argv[0], strerror(errno)), 1;
+		FILE * f = fopen(HOSTNAME_FILE, "w");
+		if (!f) return fprintf(stderr, "%s: %s: %s\n", argv[0], HOSTNAME_FILE, strerror(errno)), 1;
+		fprintf(f, "%s\n", argv[optind]);
+		fclose(f);
+		return 0;
+	}
+
+	char tmp[256] = {0};
+	if (gethostname(tmp, 255) == -1) return fprintf(stderr, "%s: gethostname: %s\n", argv[0], strerror(errno)), 1;
+	printf("%s\n", tmp);
 	return 0;
 }
