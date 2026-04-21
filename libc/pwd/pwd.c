5bd91dc97ee598286843194d28f3ff5a987ff61a
K. Lange <klange@toaruos.org>
2026-04-21 20:45:46 +0900

    libc: fgetpwent: use getline; don't allocate pw_ent

--- libc/pwd/pwd.c
+++ libc/pwd/pwd.c
@@ -37,25 +37,15 @@ static void open_it(void) {
 
 #define LINE_LEN 2048
 
-static struct passwd * pw_ent;
-static char * pw_blob;
+static struct passwd pw_ent;
+static char * pw_blob = NULL;
+static size_t pw_blob_avail = 0;
 
 struct passwd * fgetpwent(FILE * stream) {
-	if (!stream) {
-		return NULL;
-	}
-	if (!pw_ent) {
-		pw_ent = malloc(sizeof(struct passwd));
-		pw_blob = malloc(LINE_LEN);
-	}
-
-	memset(pw_blob, 0x00, LINE_LEN);
-	fgets(pw_blob, LINE_LEN, stream);
-
-
-	if (pw_blob[strlen(pw_blob)-1] == '\n') {
-		pw_blob[strlen(pw_blob)-1] = '\0'; /* erase newline */
-	}
+	size_t len;
+	if (!stream) return NULL;
+	if ((len = getline(&pw_blob, &pw_blob_avail, stream)) <= 0) return NULL;
+	if (pw_blob[len-1] == '\n') pw_blob[len-1] = '\0';
 
 	/* Tokenize */
 	char *p, *tokens[8], *last;
@@ -67,16 +57,16 @@ struct passwd * fgetpwent(FILE * stream) {
 
 	if (i < 8) return NULL;
 
-	pw_ent->pw_name    = tokens[0];
-	pw_ent->pw_passwd  = tokens[1];
-	pw_ent->pw_uid     = atoi(tokens[2]);
-	pw_ent->pw_gid     = atoi(tokens[3]);
-	pw_ent->pw_gecos   = tokens[4];
-	pw_ent->pw_dir     = tokens[5];
-	pw_ent->pw_shell   = tokens[6];
-	pw_ent->pw_comment = tokens[7];
+	pw_ent.pw_name    = tokens[0];
+	pw_ent.pw_passwd  = tokens[1];
+	pw_ent.pw_uid     = atoi(tokens[2]);
+	pw_ent.pw_gid     = atoi(tokens[3]);
+	pw_ent.pw_gecos   = tokens[4];
+	pw_ent.pw_dir     = tokens[5];
+	pw_ent.pw_shell   = tokens[6];
+	pw_ent.pw_comment = tokens[7];
 
-	return pw_ent;
+	return &pw_ent;
 }
 
 struct passwd * getpwent(void) {
@@ -97,12 +87,10 @@ void endpwent(void) {
 		fclose(pwdb);
 		pwdb = NULL;
 	}
-	if (pw_ent) {
-		free(pw_ent);
-		free(pw_blob);
-		pw_ent = NULL;
-		pw_blob = NULL;
-	}
+	free(pw_blob);
+	memset(&pw_ent, 0, sizeof(struct passwd));
+	pw_blob = NULL;
+	pw_blob_avail = 0;
 }
 
 struct passwd * getpwnam(const char * name) {
