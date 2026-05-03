5f0b8949edc58ad9024d2b4c4f08aa4b4c66fac4
K. Lange <klange@toaruos.org>
2026-05-03 15:38:13 +0900

    libc: internal shared function for calculating modes from chmod arg

---
+++ libc/unistd/mode_calc.c
@@ -0,0 +1,98 @@
+#include <sys/stat.h>
+
+mode_t __mode_calculate(const char * c, mode_t base, mode_t mask, int invert_symbolic) {
+	if (*c >= '0' && *c <= '7') {
+		mode_t out = 0;
+		while (*c >= '0' && *c <= '7') {
+			out <<= 3;
+			out |= *c - '0';
+			c++;
+		}
+
+		if (*c) return (mode_t)-1;
+		return out;
+	}
+
+	enum ModeAction { MODE_ADD, MODE_DEL, MODE_SET };
+
+	while (*c) {
+		mode_t current = base;
+		mode_t masked = 0;
+		int who = 0;
+
+_who:
+		switch (*c) {
+			case 'u': who |= S_IXUSR; c++; goto _who;
+			case 'g': who |= S_IXGRP; c++; goto _who;
+			case 'o': who |= S_IXOTH; c++; goto _who;
+			case 'a': who |= S_IXUSR | S_IXGRP | S_IXOTH; c++; goto _who;
+		}
+
+		if (who == 0) {
+			who = S_IXUSR | S_IXGRP | S_IXOTH;
+			masked = mask;
+		}
+
+_action: (void)0;
+		enum ModeAction action = MODE_SET;
+		mode_t affect = 0;
+		int setuid = 0;
+		int setgid = 0;
+		int more_actions = 0;
+
+		switch (*c) {
+			case '+': action = MODE_ADD; c++; break;
+			case '-': action = MODE_DEL; c++; break;
+			case '=': action = MODE_SET; c++; break;
+			default: goto _invalid;
+		}
+
+		switch (*c) {
+			case 'u': affect = (current & S_IRWXU) >> 6; c++; goto _apply;
+			case 'g': affect = (current & S_IRWXG) >> 3; c++; goto _apply;
+			case 'o': affect = (current & S_IRWXO) >> 0; c++; goto _apply;
+		}
+
+_permlist:
+		switch (*c) {
+			case 'r': affect |= S_IROTH; c++; goto _permlist;
+			case 'w': affect |= S_IWOTH; c++; goto _permlist;
+			case 'x': affect |= S_IXOTH; c++; goto _permlist;
+			case 's':
+				setuid = (who & S_IXUSR) ? 1 : 0;
+				setgid = (who & S_IXGRP) ? 1 : 0;
+				goto _permlist;
+			case '+': more_actions = 1; c++; break;
+			case ',': c++; break;
+			case '\0': break;
+			default: goto _invalid;
+		}
+
+_apply: (void)0;
+		mode_t applied = (affect * who) & ~masked;
+		if (setuid) applied |= S_ISUID;
+		if (setgid) applied |= S_ISGID;
+
+		switch (action) {
+			case MODE_SET:
+				current &= ~(who * S_IRWXO);
+				/* fallthrough */
+			case MODE_ADD:
+				current |= applied;
+				break;
+			case MODE_DEL:
+				current &= ~(applied);
+				break;
+		}
+
+		base = current;
+		if (more_actions) goto _action;
+	}
+
+	if (invert_symbolic) return 0777 & (~base);
+	return base;
+
+_invalid:
+	return (mode_t)-1;
+}
+
