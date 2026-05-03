8c44810375df95be4e6d8d2eee6c5a49b8296fae
K. Lange <klange@toaruos.org>
2026-05-03 17:29:10 +0900

    mkdir: more correct interpretation of modes

--- apps/mkdir.c
+++ apps/mkdir.c
@@ -4,8 +4,9 @@
  * @copyright
  * This file is part of ToaruOS and is released under the terms
  * of the NCSA / University of Illinois License - see LICENSE.md
- * Copyright (C) 2013-2014 K. Lange
+ * Copyright (C) 2013-2026 K. Lange
  */
+#include <stdlib.h>
 #include <stdio.h>
 #include <stdint.h>
 #include <string.h>
@@ -15,26 +16,31 @@
 #include <sys/stat.h>
 #include <sys/types.h>
 
-int makedir(const char * dir, int mask, int parents) {
-	if (!parents) return mkdir(dir,mask);
-
+int makedir(const char * dir, mode_t mode, mode_t mask, mode_t parent_mask, int parents) {
 	char * tmp = strdup(dir);
-	char * c = tmp;
-	while ((c = strchr(c+1,'/'))) {
-		*c = '\0';
-		if (mkdir(tmp,mask) < 0) {
-			if (errno == EEXIST) {
-				*c = '/';
-				continue;
-			} else {
-				return -1;
+	if (parents) {
+		umask(parent_mask);
+		char * c = tmp;
+		while ((c = strchr(c+1,'/'))) {
+			*c = '\0';
+			if (mkdir(tmp,0777) < 0) {
+				if (errno == EEXIST) {
+					*c = '/';
+					continue;
+				} else {
+					free(tmp);
+					return -1;
+				}
 			}
+			*c = '/';
+			continue;
 		}
-		*c = '/';
-		continue;
 	}
 
-	return mkdir(tmp, mask);
+	umask(mask);
+	int out = mkdir(tmp, mode);
+	free(tmp);
+	return out;
 }
 
 extern mode_t __mode_calculate(const char *, mode_t, mode_t, int);
@@ -44,16 +50,16 @@ int main(int argc, char ** argv) {
 	int parents = 0;
 	int opt;
 
-	mode_t mode = S_IRWXU | S_IRWXG | S_IRWXO;
+	mode_t mask = umask(0);
+	mode_t parent_mask = mask & ~(S_IWUSR | S_IXUSR);
+	mode_t mode = 0777;
 
 	while ((opt = getopt(argc, argv, "m:p")) != -1) {
 		switch (opt) {
-			case 'm': {
-				mode_t current = umask(0);
-				umask(current);
-				mode = __mode_calculate(optarg, 0777, current, 0);
+			case 'm':
+				mode = __mode_calculate(optarg, 0777, mask, 0);
+				mask = mask & ~mode;
 				break;
-			}
 			case 'p':
 				parents = 1;
 				break;
@@ -66,7 +72,7 @@ int main(int argc, char ** argv) {
 	}
 
 	for (int i = optind; i < argc; ++i) {
-		if (makedir(argv[i], mode, parents) < 0) {
+		if (makedir(argv[i], mode, mask, parent_mask, parents) < 0) {
 			if (parents && errno == EEXIST) continue;
 			fprintf(stderr, "%s: %s: %s\n", argv[0], argv[i], strerror(errno));
 			retval = 1;
