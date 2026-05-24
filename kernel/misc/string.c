b9a82f44acccbf33c345e6c243ee8666bfdec847
K. Lange <klange@toaruos.org>
2026-05-24 09:01:14 +0900

    kernel: memcpy: only use larger sizes when source and dest are aligned

--- kernel/misc/string.c
+++ kernel/misc/string.c
@@ -29,15 +29,19 @@ void * memcpy(void * restrict dest, const void * restrict src, size_t n) {
 	uint64_t * d_64 = dest;
 	const uint64_t * s_64 = src;
 
-	for (; n >= 8; n -= 8) {
-		*d_64++ = *s_64++;
+	if (((uintptr_t)d_64 & 7) == 0 && ((uintptr_t)s_64 & 7) == 0) {
+		for (; n >= 8; n -= 8) {
+			*d_64++ = *s_64++;
+		}
 	}
 
 	uint32_t * d_32 = (void*)d_64;
 	const uint32_t * s_32 = (const void*)s_64;
 
-	for (; n >= 4; n -= 4) {
-		*d_32++ = *s_32++;
+	if (((uintptr_t)d_32 & 3) == 0 && ((uintptr_t)s_32 & 3) == 0) {
+		for (; n >= 4; n -= 4) {
+			*d_32++ = *s_32++;
+		}
 	}
 
 	uint8_t * d = (void*)d_32;
