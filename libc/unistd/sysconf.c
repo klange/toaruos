72790813f42e046f280626f0a4c784ecbd16a99f
K. Lange <klange@toaruos.org>
2026-06-01 12:34:53 +0900

    libc: a few more sysconf things

--- libc/unistd/sysconf.c
+++ libc/unistd/sysconf.c
@@ -1,6 +1,7 @@
 #include <libc/syscall.h>
 #include <unistd.h>
 #include <stdlib.h>
+#include <time.h>
 #include <errno.h>
 #include <sys/signal_defs.h>
 
@@ -17,11 +18,33 @@ long sysconf(int name) {
 		case _SC_THREAD_THREADS_MAX:
 			return -1;
 
+		case _SC_NGROUPS_MAX:
+			return 32; /* defined in kernel */
+
+		case _SC_CLK_TCK:
+			return CLOCKS_PER_SEC;
+
+		case _SC_JOB_CONTROL:
+			return 1; /* Should be _POSIX_JOB_CONTROL */
+
+		case _SC_HOST_NAME_MAX:
+			return 255;
+
+		case _SC_ATEXIT_MAX:
+			return ATEXIT_MAX;
+
+		case _SC_SYMLOOP_MAX:
+			return 8; /* should be SYMLOOP_MAX */
+
 		case _SC_PAGESIZE:
 			return 4096;
 
 		case _SC_NSIG:
 			return NUMSIGNALS;
+
+		case _SC_V7_LP64_OFF64:
+		case _SC_V8_LP64_OFF64:
+			return 1;
 
 		case _SC_TTY_NAME_MAX:
 			return NAME_MAX;
@@ -31,7 +54,7 @@ long sysconf(int name) {
 			return syscall_nproc();
 
 		default:
-			errno = -ENOSYS;
+			/* For everything else, we return -1 without setting errno. */
 			return -1;
 	}
 }
