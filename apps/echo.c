fc39da66bb23f69c9e33589fa481623520285354
K. Lange <klange@toaruos.org>
2026-07-03 14:04:06 +0900

    echo: fix \r escape

--- apps/echo.c
+++ apps/echo.c
@@ -77,7 +77,7 @@ int main(int argc, char ** argv) {
 							putchar('\n');
 							break;
 						case 'r':
-							putchar('r');
+							putchar('\r');
 							break;
 						case 't':
 							putchar('\t');
