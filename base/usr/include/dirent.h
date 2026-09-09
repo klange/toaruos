f19a9aa33e069e54001643ee3dd4776980eab801
K. Lange <klange@toaruos.org>
2026-09-09 20:46:17 +0900

    libc: dirent.h: add cxx wrappers

--- base/usr/include/dirent.h
+++ base/usr/include/dirent.h
@@ -1,6 +1,11 @@
 #pragma once
 
+#include <_cheader.h>
 #include <bits/dirent.h>
+
+_Begin_C_Header
 
 int alphasort(const struct dirent ** c1, const struct dirent ** c2);
 int scandir(const char *dirname, struct dirent ***namelist, int (*select)(const struct dirent *), int (*compar)(const struct dirent **, const struct dirent **));
+
+_End_C_Header
